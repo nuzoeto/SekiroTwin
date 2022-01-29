8c03f241f25d714e0ca05b5dc20a91c36d7342d7
davitudoplugins1234 <97841570+davitudoplugins1234@users.noreply.github.com>
2022-01-29 20:16:54 -0300

    Update config.py

--- megumin/config.py
+++ megumin/config.py
@@ -8,7 +8,7 @@ import os
 
 class Config:
     AUTH_CHATS = set(
-        [-1001569084822, -1001252486871, -1001412694056, -1001475334171, -1001517679518]
+        [-1001569084822, -1001252486871, -1001412694056, -1001475334171, 1715384854, -1001517679518]
     )  # chat permitidos
     if os.environ.get("AUTH_CHATS"):
         AUTH_CHATS.update(map(int, os.environ.get("AUTH_CHATS").split()))
@@ -16,6 +16,7 @@ class Config:
         838926101,  # @fnixdev
         2138770172,  # @Luska1331
         1157759484,  # @yusukesy
+        1715384854,  # @DaviTudo 
     )
     LOG_CHANNEL_ID = set([-1001569084822])
     ADMINS = {}
