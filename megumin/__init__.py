151a5ea62a808e0224f39617273d06e1c5083909
davitudoplugins1234 <97841570+davitudoplugins1234@users.noreply.github.com>
2022-01-30 11:33:38 -0300

    Update __init__.py

--- megumin/__init__.py
+++ megumin/__init__.py
@@ -1,6 +1,10 @@
 # Copyright (C) 2022 by fnixdev
 #
 
+import time
+
+uptime = time.time()
+
 from .config import Config
 from .bot import megux, START_TIME
 from .utils.logger import logging  # nao_meche
