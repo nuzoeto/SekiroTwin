53519843237fad21eb06786cb843f2a523f20268
davitudoplugins1234 <97841570+davitudoplugins1234@users.noreply.github.com>
2022-02-03 17:23:22 -0300

    Memes

---
+++ megumin/modulos/ben.py
@@ -0,0 +1,12 @@
+from pyrogram import filters
+
+from megumin import megux
+
+@megux.on_message(filters.command(["bun"]))
+async def get_stats(_, message):
+  if message.reply_to_message:
+  sent = await message.reply("```Banindo usuario...```")
+  await asyncio.sleep(1.1)
+  await sent.edit(f"Usuário <a href='tg://user?id={message.reply_to_message.from_user.id}'>{message.reply_to_message.from_user.first_name}</a>  foi banido por <a href='tg://user?id={message.from_user.id}'>{message.from_user.first_name}</a> no chat: **{message.chat.title}**")
+  else:
+    await message.reply("Marque a mensagem de um usuario!")
