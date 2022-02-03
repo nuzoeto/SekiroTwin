5a2c108f2ea879cf57a6d4e01470abcf565dd9d4
davitudoplugins1234 <97841570+davitudoplugins1234@users.noreply.github.com>
2022-02-03 17:45:58 -0300

    Update ben.py

--- megumin/modulos/ben.py
+++ megumin/modulos/ben.py
@@ -12,6 +12,7 @@ from megumin import megux
 
 @megux.on_message(filters.command(["bun"]))
 async def get_stats(_, message):
+
   sent = await message.reply("```Banindo usuario...```")
   await asyncio.sleep(1.1)
   await sent.edit(f"Usuário <a href='tg://user?id={message.reply_to_message.from_user.id}'>{message.reply_to_message.from_user.first_name}</a>  foi banido por <a href='tg://user?id={message.from_user.id}'>{message.from_user.first_name}</a> no chat: **{message.chat.title}**")
