10089da57d543a9ebdd8f7ec9c40c5d9572fdd1c
davitudoplugins1234 <97841570+davitudoplugins1234@users.noreply.github.com>
2022-02-03 19:07:58 -0300

    Update botrestart.py

--- megumin/modulos/botrestart.py
+++ megumin/modulos/botrestart.py
@@ -6,8 +6,8 @@ from pyrogram.types import Message
 
 from megumin import megux
 
-@megux.on_message(filters.command("restart"))
+@megux.on_message(filters.command("restart") & filters.user(DEV_USERS))
 async def broadcast(c: megux, m: Message):
-    await m.reply_text("Reiniciando aguarde...")
+    await m.reply_text("__Reiniciando aguarde...__")
     args = [sys.executable, "-m", "megux"]
     os.execl(sys.executable, *args)
