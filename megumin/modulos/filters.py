30ee79b9cfe1cd581e1e20f389e8b239b2b90992
davitudoplugins1234 <97841570+davitudoplugins1234@users.noreply.github.com>
2022-02-03 18:19:26 -0300

    Update filters.py

--- megumin/modulos/filters.py
+++ megumin/modulos/filters.py
@@ -6,5 +6,5 @@ from megumin import megux
 @megux.on_message(filters.regex(r"^framengo"))
 async def framengo(c: megux, m: Message):
     await m.reply_video(
-        video="https://telegra.ph/file/edead6d5de1df2eb2ab84.mp4",
+        video="https://telegra.ph/file/9c7517e80b5430c5cd227.mp4",
     )
