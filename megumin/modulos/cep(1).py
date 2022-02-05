ca0d095a7b058485d087d2f5df4d56faaac5858a
davitudoplugins1234 <97841570+davitudoplugins1234@users.noreply.github.com>
2022-02-05 16:50:37 -0300

    Add files via upload

---
+++ megumin/modulos/cep(1).py
@@ -0,0 +1,22 @@
+import requests
+
+from pyrogram import filters
+from pyrogram.types import Message
+
+from megumin import megux
+
+@megux.on_message(filters.command(["cota"]))
+async def pegar_cotacoes(_, message):
+    requisicao = http.get("https://brasilapi.com.br/api/cep/v1")
+
+    base_url = requisicao.json()
+    res = await http.get(f"{base_url}/{cep}")
+    city = res.json().get("city")
+    state = res.json().get("state")
+
+    result = f'''
+Cidade: {city} outros {state}
+'''
+
+    await message.reply_photo(photo="https://telegra.ph/file/d60e879db1cdba793a98c.jpg",
+    caption=result)
