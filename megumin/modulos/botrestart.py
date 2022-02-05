6b7e4fc86838bb9a78d46de083ec421516a9b74b
davitudoplugins1234 <97841570+davitudoplugins1234@users.noreply.github.com>
2022-02-04 23:01:53 -0300

    Update botrestart.py

--- megumin/modulos/botrestart.py
+++ megumin/modulos/botrestart.py
@@ -15,7 +15,7 @@ from megumin import megux
 @megux.on_message(filters.command("restart") & filters.user(1715384854))
 async def broadcast(c: megux, m: Message):
     sent = await m.reply("__Reiniciando aguarde...__")
-    await asyncio.sleep(21)
+    await asyncio.sleep(24.2)
     await sent.edit("**WhiterKang Reiniciado com Sucesso!**") 
     args = [sys.executable, "-m", "megumin"]
     os.execl(sys.executable, *args)
