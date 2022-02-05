4b30d3f4d26976909a8c92c5ba429c6b3f2661c2
davitudoplugins1234 <97841570+davitudoplugins1234@users.noreply.github.com>
2022-02-05 19:00:52 -0300

    Add files via upload

---
+++ megumin/modulos/admin/delete.py
@@ -0,0 +1,34 @@
+from pyrogram import filters
+from pyrogram.errors import MessageDeleteForbidden
+from pyrogram.types import Message
+
+from megumin import megux
+from megumin.utils import admin_check
+
+
+@megux.on_message(filters.command("purge"))
+async def purge_command(megux, message: Message):
+  msg_ids = [message.message_id]
+    can_purge = await admin_check(message)
+    if can_purge:
+        try:
+            message_reply = int(message.reply_to_message.message_id)
+        except AttributeError:
+            await message.reply(
+                "Por favor, marque a mensagem que deseja apagar."
+            )
+            return
+
+        while True:
+            try:
+                await megux.delete_messages(message.chat.id, msg_ids
+            except MessageDeleteForbidden:
+                await message.reply(
+                    "Eu não tenho as seguintes permissões: **Can delete messages**.  "
+                )
+                return
+            except Exception as exc:
+                await message.reply(f"ERRO: {exc}")
+                return
+    else:
+        await message.reply("👮‍♀ Você precisa ser um administrador!")
