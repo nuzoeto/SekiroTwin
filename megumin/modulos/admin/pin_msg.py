e083fc3257cc7a57e20e7c2b3b9195b33173ef57
davitudoplugins1234 <97841570+davitudoplugins1234@users.noreply.github.com>
2022-02-06 18:02:42 -0300

    Update pin_msg.py

--- megumin/modulos/admin/pin_msg.py
+++ megumin/modulos/admin/pin_msg.py
@@ -35,4 +35,4 @@ async def unpin_(_, message: Message):
         await message.reply("`Responda a uma mensagem para que eu possa desfixa-la`")
         return
     await message.reply_to_message.unpin()
-    await message.reply(f"""Eu desfixei essa </b> <a href='t.me/{message.chat.username}/{message.reply_to_message.message_id}'>mensagem</a>""")
+    await message.reply(f"""Eu desfixei essa </b> <a href='t.me/{message.chat.username}/{message.reply_to_message.message_id}'>mensagem</a>""", quote=True)
