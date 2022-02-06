cb5913d3afb882cb99e0af6b21032ec0e4b03f91
davitudoplugins1234 <97841570+davitudoplugins1234@users.noreply.github.com>
2022-02-06 12:00:02 -0300

    Update pin_msg.py

--- megumin/modulos/admin/pin_msg.py
+++ megumin/modulos/admin/pin_msg.py
@@ -8,6 +8,7 @@ from megumin.utils import check_bot_rights, check_rights
 @megux.on_message(filters.command("pin"))
 async def pin_(_, message: Message):
     chat_id = message.chat.id
+    msg = message.reply_to_message or message
     if not await check_rights(chat_id, message.from_user.id, "can_pin_messages"):
         await reply("`Você não tem permissão pra fazer isso`")
         return
