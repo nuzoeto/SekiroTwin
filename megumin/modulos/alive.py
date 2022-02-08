b65774b5d93de69d04d8787a7d148743c19cb1d6
davitudoplugins1234 <97841570+davitudoplugins1234@users.noreply.github.com>
2022-02-07 23:10:58 -0300

    Update alive.py

--- megumin/modulos/alive.py
+++ megumin/modulos/alive.py
@@ -5,8 +5,7 @@ from megumin import megux, version
 
 @megux.on_message(filters.command(["alive"]))
 async def start(_, message):
-    chats = await megux.get_send_as_chats(chat_id)
-    text= f"""ᴏɪ ᴇᴜ ᴇsᴛᴏᴜ ᴠɪᴠᴏ!\n**• Versão do Bot **: {version.__megumin_version__}\n**• Versão do Python **: {version.__python_version__}\n**• Versão do Pyrogram **: {version.__pyro_version__} os vejo em {chats}"""
+    text= f"""ᴏɪ ᴇᴜ ᴇsᴛᴏᴜ ᴠɪᴠᴏ!\n**• Versão do Bot **: {version.__megumin_version__}\n**• Versão do Python **: {version.__python_version__}\n**• Versão do Pyrogram **: {version.__pyro_version__}"""
     
     keyboard = InlineKeyboardMarkup(
                             [
