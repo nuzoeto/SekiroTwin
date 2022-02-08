439615ac5e2b68956229c469b2ec0c4d58870dc3
davitudoplugins1234 <97841570+davitudoplugins1234@users.noreply.github.com>
2022-02-08 18:26:17 -0300

    Update cotação.py

--- megumin/modulos/cotação.py
+++ megumin/modulos/cotação.py
@@ -13,22 +13,28 @@ async def pegar_cotacoes(_, message):
 
     cotacao_dolar = requisicao_dic['USDBRL']['bid']
     dat_dolar = requisicao_dic ['USDBRL']['create_date']
+    var_dolar = requisicao_dic ['USDBRL']['varBid']
     cotacao_euro = requisicao_dic['EURBRL']['bid']
     dat_euro = requisicao_dic['EURBRL']['create_date']
+    var_euro = requisicao_dic ['EURBRL']['varBid']
     cotacao_btc = requisicao_dic['BTCBRL']['bid']
     dat_btc = requisicao_dic['BTCBRL']['create_date']
+    var_btc = requisicao_dic ['USDBRL']['varBid']
 
     result = f'''
 **Cotação das moedas:**
 
 💵 **Dólar:** R$ ```{cotacao_dolar}```
 🗓 **Data:**  ```{dat_dolar}```
+📊 **Variação:** ```{var_dolar}```
 
 💵 **Euro:** R$ ```{cotacao_euro}```
 🗓 **Data:**  ```{dat_euro}```
+📊 **Variação:** ```{var_euro}```
 
 💵 **BTC:** R$ ```{cotacao_btc}```
-🗓 **Data:**  ```{dat_btc}```'''
+🗓 **Data:**  ```{dat_btc}```
+📊 **Variação:** ```{var_btc}```'''
 
     await message.reply_photo(photo="https://telegra.ph/file/d60e879db1cdba793a98c.jpg",
     caption=result)
