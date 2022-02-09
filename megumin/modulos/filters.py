0955a7f0af45aaaa63ac95b8bf2b591743372ad7
davitudoplugins1234 <97841570+davitudoplugins1234@users.noreply.github.com>
2022-02-09 17:27:32 -0300

    Update filters.py

--- megumin/modulos/filters.py
+++ megumin/modulos/filters.py
@@ -18,7 +18,7 @@ async def framengo(c: megux, m: Message):
 @megux.on_message(filters.regex(r"^Olá",r"^Ola",r"^olá",r"^ola", r"^Oi", r"^Oi,Tudo bem?"))
 async def complimentacao_(c: megux, m: Message):
   if m.chat.type == "private":
-ola = random.choice(OLA_STRING)
-    await m.reply(f"__{ola}__")
+sla = random.choice(OI)
+    await m.reply(f"__{sla}__")
 
-OLA_STRING = [f"""Olá! Como posso ajudar {message.from_user.first_name}""", "Olá! Tudo bem com você?", "Oi, tudo bom?", "Olá! Tudo bem com você? Posso ajudar?"]
+OI = [f"""Olá! Como posso ajudar {message.from_user.first_name}""", "Olá! Tudo bem com você?", "Oi, tudo bom?", "Olá! Tudo bem com você? Posso ajudar?"]
