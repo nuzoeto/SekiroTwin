a0d154bec839a43a935c089cd4f2e295844fb3bb
davitudoplugins1234 <97841570+davitudoplugins1234@users.noreply.github.com>
2022-02-10 19:23:04 -0300

    Update purge.py

--- megumin/modulos/admin/purge.py
+++ megumin/modulos/admin/purge.py
@@ -8,7 +8,7 @@ from megumin.utils import admin_check
 
 @megux.on_message(filters.command("purge"))
 async def purge_command(megux, message: Message):
-    await message.reply("```Apagando as mensagens...```")
+    limpando = await message.reply("```Apagando as mensagens...```")
     can_purge = await admin_check(message)
     if can_purge:
         try:
@@ -21,7 +21,7 @@ async def purge_command(megux, message: Message):
 
         while True:
             try:
-                await message.edit("**Limpeza completa!**")
+                await limpando.edit("**Limpeza completa!**")
                 await megux.delete_messages(message.chat.id, message_reply)
                 message_reply += 1
             except MessageDeleteForbidden:
