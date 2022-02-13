d656d6a2bb5b53fc16d63e29e39cc2f97582e0ca
davitudoplugins1234 <97841570+davitudoplugins1234@users.noreply.github.com>
2022-02-13 17:24:08 -0300

    Update whois.py

--- megumin/modulos/whois.py
+++ megumin/modulos/whois.py
@@ -21,7 +21,7 @@ infotext = (
 
 def LastOnline(user: User):
     if user.is_bot:
-        return "bot"
+        return "usuário é bot"
     elif user.status == "recently":
         return "Recentemente"
     elif user.status == "within_week":
