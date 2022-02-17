803a99cee7eed36388dd4cb161b61a266dcf8055
davitudoplugins1234 <97841570+davitudoplugins1234@users.noreply.github.com>
2022-02-17 17:17:39 -0300

    Add files via upload

---
+++ megumin/modulos/ddd.py
@@ -0,0 +1,29 @@
+import os
+import httpx
+import rapidjson
+
+from pyrogram import filters
+from pyrogram.types import Message
+from pyrogram.errors import BadRequest
+
+from megumin import megux
+
+ http = httpx.AsyncClient()
+
+@megux.on_message(filters.command(["ddd"]))
+async def lastfm(c: megux, m: Message):
+    try:
+            ddd = m.text.split(maxsplit=1)[1]
+    except IndexError:
+        await m.reply_text("DDD nao inserido!")
+        return
+
+    base_url = "https://brasilapi.com.br/api/ddd/v1"
+    res = await http.get(f"{base_url}/{ddd}")
+    state = res.json().get("state")
+    states = await http.get(f"https://brasilapi.com.br/api/ibge/uf/v1/{state}")
+    state_name = states.json().get("nome")
+    cities = res.json().get("cities")
+    
+    rep =f"📞 <b>DDD - {ddd}</b> \n<b>- Estado:</b> {state_name} - {state}\n\n **Cidades\n{cities}"
+    await message.reply(rep)
