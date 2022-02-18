6320b89cf0f836f7b2f959811a4994ff21b14e13
davitudoplugins1234 <97841570+davitudoplugins1234@users.noreply.github.com>
2022-02-18 18:28:44 -0300

    Update warnfake.py

--- megumin/modulos/warnfake.py
+++ megumin/modulos/warnfake.py
@@ -6,10 +6,10 @@ from pyrogram.types import Message
 from megumin import megux
 
 @megux.on_message(filters.command(["warm"]))
-async def printer(_, message: Message):
+async def printer(_, m: Message):
     warmf = random.choice(rep)
     await message.reply(f"{warmf}")
 
-rep = [f"**<a href='tg://user?id={message.reply_to_message.from_user.id}'>{message.reply_to_message.from_user.first_name}</a> foi advertido!**\nEle(a) têm 1/3 Advertencias.",
-f"**<a href='tg://user?id={message.reply_to_message.from_user.id}'>{message.reply_to_message.from_user.first_name}</a> foi advertido!**\nEle(a) têm 2/3 Advertencias.", 
-f"**<a href='tg://user?id={message.reply_to_message.from_user.id}'>{message.reply_to_message.from_user.first_name}</a> foi advertido!**\nEle(a) têm 3/3 Advertencias **esta banido**" ]
+rep = [f"**<a href='tg://user?id={m.reply_to_message.from_user.id}'>{message.reply_to_message.from_user.first_name}</a> foi advertido!**\nEle(a) têm 1/3 Advertencias.",
+f"**<a href='tg://user?id={m.reply_to_message.from_user.id}'>{message.reply_to_message.from_user.first_name}</a> foi advertido!**\nEle(a) têm 2/3 Advertencias.", 
+f"**<a href='tg://user?id={m.reply_to_message.from_user.id}'>{message.reply_to_message.from_user.first_name}</a> foi advertido!**\nEle(a) têm 3/3 Advertencias **esta banido**" ]
