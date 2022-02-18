7c502a8bb931b1f022998dc3faec3573933832ae
davitudoplugins1234 <97841570+davitudoplugins1234@users.noreply.github.com>
2022-02-18 18:16:50 -0300

    Teste

---
+++ megumin/modulos/warnfake.py
@@ -0,0 +1,15 @@
+import random
+
+from pyrogram import filters
+from pyrogram.types import Message
+
+from megumin import megux
+
+@megux.on_message(filters.command(["warm"]))
+async def printer(_, message: Message):
+    warmf = random.choice(rep)
+    await m.reply(f"{warmf}")
+
+rep = [f"**<a href='tg://user?id={message.reply_to_message.from_user.id}'>{message.reply_to_message.from_user.first_name}</a> foi advertido!**\nEle(a) têm 1/3 Advertencias.",
+f"**<a href='tg://user?id={message.reply_to_message.from_user.id}'>{message.reply_to_message.from_user.first_name}</a> foi advertido!**\nEle(a) têm 2/3 Advertencias.", 
+f"**<a href='tg://user?id={message.reply_to_message.from_user.id}'>{message.reply_to_message.from_user.first_name}</a> foi advertido!**\nEle(a) têm 3/3 Advertencias **esta banido**" ]
