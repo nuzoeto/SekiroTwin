b4edb4cf4cd41399a910046f14ab2e222b8117f8
davitudoplugins1234 <97841570+davitudoplugins1234@users.noreply.github.com>
2022-02-18 18:24:04 -0300

    Update warnfake.py

--- megumin/modulos/warnfake.py
+++ megumin/modulos/warnfake.py
@@ -1,14 +1,13 @@
 import random
 
 from pyrogram import filters
-from pyrogram.types import Message
 
 from megumin import megux
 
 @megux.on_message(filters.command(["warm"]))
-async def printer(_, m: Message):
+async def printer(_, message):
     warmf = random.choice(rep)
-    await m.reply(f"{warmf}")
+    await message.reply(f"{warmf}")
 
 rep = [f"**<a href='tg://user?id={message.reply_to_message.from_user.id}'>{message.reply_to_message.from_user.first_name}</a> foi advertido!**\nEle(a) têm 1/3 Advertencias.",
 f"**<a href='tg://user?id={message.reply_to_message.from_user.id}'>{message.reply_to_message.from_user.first_name}</a> foi advertido!**\nEle(a) têm 2/3 Advertencias.", 
