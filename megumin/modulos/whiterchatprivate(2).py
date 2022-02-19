51c2d8b7575080530a78829d6a284a23f063c5da
davitudoplugins1234 <97841570+davitudoplugins1234@users.noreply.github.com>
2022-02-19 14:45:39 -0300

    Update whiterchatprivate(2).py

--- megumin/modulos/whiterchatprivate(2).py
+++ megumin/modulos/whiterchatprivate(2).py
@@ -9,7 +9,7 @@ from megumin import megux
 
 @megux.on_message(filters.private)
 async def chatbot_(c: megux, message: Message):
-  if "Oi", "oi" in message.text:
+  if "Oi" in message.text:
     await message.reply("Oi, tudo bom?")
   else:
     return
