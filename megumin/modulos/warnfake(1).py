25a70b664dbbfd5e915e9f21ff41c9da47962121
davitudoplugins1234 <97841570+davitudoplugins1234@users.noreply.github.com>
2022-02-20 10:59:57 -0300

    Update warnfake(1).py

--- megumin/modulos/warnfake(1).py
+++ megumin/modulos/warnfake(1).py
@@ -6,7 +6,7 @@ from pyrogram.types import Message
 from megumin import megux
 
 @megux.on_message(filters.command(["warm"]))
-async def printer(message: Message):
+async def printer(c: megux, message: Message):
     warmf = random.choice(rep)
     await m.reply(f"{warmf}")
 
