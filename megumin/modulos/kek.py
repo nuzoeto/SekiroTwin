ae05daad15ce56a6d20b6599aef47d99bb342ea0
davitudoplugins1234 <97841570+davitudoplugins1234@users.noreply.github.com>
2022-02-20 11:18:59 -0300

    Create kek.py

---
+++ megumin/modulos/kek.py
@@ -0,0 +1,21 @@
+import os
+
+from pyrogram import filters
+from pyrogram.types import Message
+from pyrogram.errors import BadRequest
+
+from megumin import megux
+
+
+@megux.on_message(filters.command(["kek"]))
+async def lastfm(c: megux, m: Message):
+    try:
+         kek = m.text.split(maxsplit=1)[1]
+    except IndexError:
+        await m.reply_text("__Kek..__")
+        return
+
+Hmm = __{kek}__
+
+
+await message.reply(Hmm)
