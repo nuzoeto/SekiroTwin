5b0ee01d136aa26b7665ce7dba7b10c15b4e34a2
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-02-24 20:55:47 -0300

    ++
    Alterado devido a filter prefixes inválido!

--- megumin/modulos/admin/pin_msg.py
+++ megumin/modulos/admin/pin_msg.py
@@ -39,7 +39,7 @@ async def pin_(_, message: Message):
     await message.reply(f"""Eu fixei</b> <a href='t.me/{message.chat.username}/{message.reply_to_message.message_id}'>esta mensagem</a> e notifiquei todos os membros.""", disable_web_page_preview=True, disable_notification=False)
 
 
-@megux.on_message(filters.command("unpin", prefixes=["/", "!"))
+@megux.on_message(filters.command("unpin", prefixes=["/", "!"]))
 async def unpin_(_, message: Message):
     chat_id = message.chat.id
     if not await check_rights(chat_id, message.from_user.id, "can_pin_messages"):
