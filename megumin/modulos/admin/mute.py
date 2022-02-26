0aa8f47db7d958d0b36b1f2f387f0d33a8234c9a
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-02-26 10:04:17 -0300

    Update mute.py

--- megumin/modulos/admin/mute.py
+++ megumin/modulos/admin/mute.py
@@ -182,8 +182,7 @@ async def _unmute_user(_, message: Message):
 async def muteme_(_, message: Message):
     chat_id = message.chat.id
     user_id = message.from_user.id
-    admin_ = await is_admin(message)
-    if admin_:
+    if is_admin(chat_id, user_id):
         await message.reply("Por que eu mutaria um(a) administrador(a)? Parece uma ideia bem idiota.")
         return
     else:
