002eea6de978caf87ab921a19d628a9edaa235f4
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-02-26 20:51:03 -0300

    Update pin_msg.py

--- megumin/modulos/admin/pin_msg.py
+++ megumin/modulos/admin/pin_msg.py
@@ -19,7 +19,7 @@ async def pin_(_, message: Message):
         await message.reply("Responda a uma mensagem para que eu possa fixa-la")
         return
     await message.reply_to_message.pin()
-    await message.reply(f"""Eu fixei</b> <a href='t.me/c/{ids_chat}/{message.reply_to_message.message_id}'>esta mensagem</a>.""", disable_web_page_preview=True, disable_notification=True)
+    await message.reply(f"""__Eu fixei</b> <a href='t.me/c/{ids_chat}/{message.reply_to_message.message_id}'>esta mensagem</a>.__""", disable_web_page_preview=True, disable_notification=True)
 
 
 @megux.on_message(filters.command("loudpin", prefixes=["/", "!"]))
@@ -36,7 +36,7 @@ async def pin_(_, message: Message):
         await message.reply("Responda a uma mensagem para que eu possa fixa-la")
         return
     await message.reply_to_message.pin()
-    await message.reply(f"""Eu fixei</b> <a href='t.me/c/{ids_chat}/{message.reply_to_message.message_id}'>esta mensagem</a> e notifiquei todos os membros.""", disable_web_page_preview=True, disable_notification=False)
+    await message.reply(f"""__Eu fixei</b> <a href='t.me/c/{ids_chat}/{message.reply_to_message.message_id}'>esta mensagem</a> e notifiquei todos os membros.__""", disable_web_page_preview=True, disable_notification=False)
 
 
 @megux.on_message(filters.command("unpin", prefixes=["/", "!"]))
@@ -53,4 +53,4 @@ async def unpin_(_, message: Message):
         await message.reply("`Responda a uma mensagem para que eu possa desfixa-la`")
         return
     await message.reply_to_message.unpin()
-    await message.reply(f"""Eu desfixei</b> <a href='t.me/c/{ids_chat}/{message.reply_to_message.message_id}'>esta mensagem</a>.""", disable_web_page_preview=True)
+    await message.reply(f"""__Eu desfixei</b> <a href='t.me/c/{ids_chat}/{message.reply_to_message.message_id}'>esta mensagem</a>.__""", disable_web_page_preview=True)
