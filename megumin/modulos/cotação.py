1205267ad584dda3979382017a12f4c965294979
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-02-27 15:02:52 -0300

    Update cotação.py

--- megumin/modulos/cotação.py
+++ megumin/modulos/cotação.py
@@ -8,7 +8,7 @@ from megumin import megux
 
 @megux.on_message(filters.command(["cota"], prefixes=["/", "!"]))
 async def pegar_cotacoes(_, message):
-    requisicao = requests.get("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,GBP-BRL,JPY-BRL,BTC-BRL,ETH-BRL,XRP-BRL,DOGE-BRL")
+    requisicao = requests.get("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,GBP-BRL,JPY-BRL,BTC-BRL,ETH-BRL,XRP-BRL,DOGE-BRL,ARS-BRL")
 
     requisicao_dic = requisicao.json()
 
@@ -24,6 +24,12 @@ async def pegar_cotacoes(_, message):
     cotacao_iene = requisicao_dic ['JPYBRL']['bid']
     dat_iene = requisicao_dic ['JPYBRL']['create_date']
     var_iene = requisicao_dic ['JPYBRL']['varBid']
+    cotacao_doge = requisicao_dic ['DOGEBRL']['bid']
+    dat_doge = requisicao_dic ['DOGEBRL']['create_date']
+    var_doge = requisicao_dic ['DOGEBRL']['varBid']
+    cotacao_ars = requisicao_dic ['ARSBRL']['bid']
+    dat_ars = requisicao_dic ['ARSBRL']['create_date']
+    var_ars = requisicao_dic ['ARSBRL']['varBid']
 
     obting_info = await message.reply(f"""```Obtendo informações sobre as moedas...```""")
     await asyncio.sleep(0.3)
@@ -37,20 +43,35 @@ async def pegar_cotacoes(_, message):
 
 📊 **Variação:** ```{var_dolar}```
 
+
 💵 **Euro:** R$ ```{cotacao_euro}```
 🗓 **Data:**  ```{dat_euro}```
 
 📊 **Variação:** ```{var_euro}```
+
 
 💵 **BTC:** R$ ```{cotacao_btc}```
 🗓 **Data:**  ```{dat_btc}```
 
 📊 **Variação:** ```{var_btc}```
 
+
+💵 **DOGE:** R$ ```{cotacao_doge}
+🗓 **Data:** ```{dat_doge}
+
+📊 **Variação:** ```{var_doge}```
+
+
 💵 **Iene:** R$ ```{cotacao_iene}```
 🗓 **Data:** ```{dat_iene}```
 
 📊 **Variação:** ```{var_iene}```
+
+
+💵 **Peso Argentino:** R$ ```{cotacao_ars}```
+🗓 **Data:** ```{dat_ars}```
+
+📊 **Variação:** ```{var_ars}```
 '''
 
     await message.reply_photo(photo="https://telegra.ph/file/d60e879db1cdba793a98c.jpg",
