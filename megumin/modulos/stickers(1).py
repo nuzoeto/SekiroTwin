52de7630c73337068dd8074856cf2b40705edfb5
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-03-02 11:56:04 -0300

    Added /getsticker

---
+++ megumin/modulos/stickers(1).py
@@ -0,0 +1,35 @@
+import tempfile
+import os
+import shutil
+
+
+from pyrogram import filters
+from pyrogram.types import Message
+
+from megumin import megux
+
+
+@megux.on_message(filters.command(["getsticker"], prefixes=["/", "!"]))
+async def getsticker_(c: megux, m: Message):
+    sticker = m.reply_to_message.sticker
+
+    if sticker:
+        if sticker.is_animated:
+            await m.reply_text("Sticker animado não é suportado!")
+        elif not sticker.is_animated:
+            with tempfile.TemporaryDirectory() as tempdir:
+                path = os.path.join(tempdir, "getsticker")
+            sticker_file = await c.download_media(
+                message=m.reply_to_message,
+                file_name=f"{path}/{sticker.set_name}.png",
+            )
+            await m.reply_to_message.reply_document(
+                document=sticker_file,
+                caption=(
+                    f"<b>Emoji:</b> {sticker.emoji}\n"
+                    f"<b>Sticker ID:</b> <code>{sticker.file_id}</code>"
+                ),
+            )
+            shutil.rmtree(tempdir, ignore_errors=True)
+    else:
+        await m.reply_text("Isso não é um sticker!")
