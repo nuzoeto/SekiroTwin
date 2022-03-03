ada4615687fe0adb8075a8c5de6e85ed7a59491a
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-03-03 16:49:54 -0300

    Add files via upload

---
+++ megumin/modulos/speed test.py
@@ -0,0 +1,31 @@
+import speedtest
+import os
+
+from pyrogram import filters
+from pyrogram.types import Message
+
+from megumin import megux
+
+@megux.on_message(filters.command("speedtest"))
+async def test_speed(c: megux, m: Message):
+    string = "<b>Teste de velocidade</b>\n\n<b>🌐 Host:</b> <code>{host}</code>\n\n<b>🏓 Latência:</b> <code>{ping} ms</code>\n<b>⬇️ Download:</b> <code>{download} Mbps</code>\n<b>⬇️ Upload:</b> <code>{upload} Mbps</code>"
+    sent = await m.reply_text(string.format(host="", ping="", download="", upload=""))
+    s = speedtest.Speedtest()
+    bs = s.get_best_server()
+    await sent.edit_text(
+        string.format(
+            host=bs["sponsor"], ping=int(bs["latency"]), download="", upload=""
+        )
+    )
+    dl = round(s.download() / 1024 / 1024, 2)
+    await sent.edit_text(
+        string.format(
+            host=bs["sponsor"], ping=int(bs["latency"]), download=dl, upload=""
+        )
+    )
+    ul = round(s.upload() / 1024 / 1024, 2)
+    await sent.edit_text(
+        string.format(
+            host=bs["sponsor"], ping=int(bs["latency"]), download=dl, upload=ul
+        )
+    )
