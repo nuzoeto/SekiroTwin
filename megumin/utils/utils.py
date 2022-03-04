3b3bd449b068ba4c4bd7fa5a91ffe1dd4622994c
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-03-04 20:08:10 -0300

    Create utils.py

---
+++ megumin/utils/utils.py
@@ -0,0 +1,21 @@
+import re
+import asyncio 
+import httpx
+
+from pyrogram import emoji
+
+def get_emoji_regex():
+    e_list = [
+        getattr(emoji, e).encode("unicode-escape").decode("ASCII")
+        for e in dir(emoji)
+        if not e.startswith("_")
+    ]
+    # to avoid re.error excluding char that start with '*'
+    e_sort = sorted([x for x in e_list if not x.startswith("*")], reverse=True)
+    # Sort emojis by length to make sure multi-character emojis are
+    # matched first
+    pattern_ = f"({'|'.join(e_sort)})"
+    return re.compile(pattern_)
+
+
+EMOJI_PATTERN = get_emoji_regex()
