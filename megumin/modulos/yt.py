f38d879cadb5f462b5eb177d1712b97b80fc2222
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-03-04 18:16:56 -0300

    Update yt.py

--- megumin/modulos/yt.py
+++ megumin/modulos/yt.py
@@ -9,6 +9,7 @@ import rapidjson
 from pyrogram.types import Message
 from pyrogram.errors import BadRequest
 from pyrogram import filters
+from youtubesearchpython import Search, SearchVideos
 
 from megumin import megux
 
@@ -16,7 +17,7 @@ from megumin import megux
 async def yt_search_cmd(c: megux, m: Message):
     vids = [
         '{}: <a href="{}">{}</a>'.format(num + 1, i["url"], i["title"])
-        for num, i in enumerate(await youtube_search(m.text.split(None, 1)[1]))
+        for num, i in enumerate(await youtube-search-python(m.text.split(None, 1)[1]))
     ]
     await m.reply_text(
         "\n".join(vids) if vids else r"¯\_(ツ)_/¯", disable_web_page_preview=True
