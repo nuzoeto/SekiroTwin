8ff78ad0f4d84f7c91f0637d2a44b31267f233a4
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-03-05 09:45:24 -0300

    Update speed test.py

--- megumin/modulos/speed test.py
+++ megumin/modulos/speed test.py
@@ -10,19 +10,19 @@ from megumin import megux
 @megux.on_message(filters.command("speedtest"))
 async def test_speed(c: megux, m: Message):
     string = "<b>Teste de velocidade</b>\n\n💻<b>ISP</b> <code>{isp}<code>\n<b>🌐 Host:</b> <code>{host}</code>\n\n<b>🏓 Latência:</b> <code>{ping} ms</code>\n<b>⬇️ Download:</b> <code>{download} Mbps</code>\n<b>⬆️ Upload:</b> <code>{upload} Mbps</code>"
-    sent = await m.reply(string.format(host="", ping="", download="", upload="", isp=""))
+    sent = await m.reply(string.format(host="", ping="", download="", upload="", isp=result["client"]["isp"]))
     s = speedtest.Speedtest()
     bs = s.get_best_server()
     result = s.results.dict()
     await sent.edit_text(
         string.format(
-            host=bs["sponsor"], ping=int(bs["latency"]), download="", upload="", isp=""
+            host=bs["sponsor"], ping=int(bs["latency"]), download="", upload="", isp=result["client"]["isp"]
         )
     )
     dl = round(s.download() / 1024 / 1024, 2)
     await sent.edit_text(
         string.format(
-            host=bs["sponsor"], ping=int(bs["latency"]), download=dl, upload="", isp=""
+            host=bs["sponsor"], ping=int(bs["latency"]), download=dl, upload="", isp=result["client"]["isp"]
         )
     )
     ul = round(s.upload() / 1024 / 1024, 2)
