ae18197e19d092898c50706df68c01a87f015ba4
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-03-06 13:22:01 -0300

    Add files via upload

---
+++ megumin/modulos/admin_list.py
@@ -0,0 +1,16 @@
+from pyrogram import filters
+from pyrogram.types import Message
+
+from megumin import megux
+
+
+@megux.on_message(filters.command("admins", prefixes=["/", "!"]) & filters.group)
+async def mentionadmins(c: megux, m: Message):
+    mention = ""
+    async for i in c.iter_chat_members(m.chat.id, filter="administrators"):
+        if not (i.user.is_deleted or i.is_anonymous):
+            mention += f"{i.user.mention}\n"
+    await c.send_message(
+        m.chat.id,
+        "<b>Administradores no chat</b> <code>{chat_title}</code>:\n{admins_list}".format(chat_title=m.chat.title, admins_list=mention),
+    )
