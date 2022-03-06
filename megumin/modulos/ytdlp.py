3d5a5e6e830f86632e269c118643e1381f027d08
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-03-06 11:03:48 -0300

    Update ytdlp.py

--- megumin/modulos/ytdlp.py
+++ megumin/modulos/ytdlp.py
@@ -13,7 +13,7 @@ from megumin import megux
 
 @megux.on_message(filters.command("ytdl", prefixes=["/","!"]))
 async def ytdl_(c: megux, m: Message):
-    args = m.matches[0]["text"]
+    args = " ".join(message.text.split()[1:])
     user = m.from_user.id
 
     if m.reply_to_message and m.reply_to_message.text:
