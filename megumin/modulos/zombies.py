c0145423ae4fd5011e44ae9d3297bc6b252e7dac
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-03-06 14:06:24 -0300

    Added deleted acount ban command

---
+++ megumin/modulos/zombies.py
@@ -0,0 +1,39 @@
+import os
+import re
+
+from pyrogram import filters
+from pyrogram.types import Message
+
+from megumin import megux
+
+
+@megux.on_message(filters.command(["zombies", "cleanup"], prefixes=["/", "!"]))
+async def cleanup(c: megux, m: Message):
+    if m.chat.type == "private":
+        await m.reply_text("Este comando é para ser usado em grupos!")
+        return
+
+    member = await c.get_chat_member(chat_id=m.chat.id, user_id=m.from_user.id)
+    if member.status in ["administrator", "creator"]:
+        deleted = []
+        sent = await m.reply_text("Iniciando limpeza...")
+        async for t in c.iter_chat_members(chat_id=m.chat.id, filter="all"):
+            if t.user.is_deleted:
+                try:
+                    await c.kick_chat_member(m.chat.id, t.user.id)
+                    deleted.append(t)
+                except BadRequest:
+                    pass
+                except Forbidden as e:
+                    await m.reply_text(
+                        f"Eu estou impedido de executar este comando! >-<\n<b>Erro:</b> <code>{e}</code>"
+                    )
+                    return
+        if deleted:
+            await sent.edit_text(
+                f"Removi todas as {len(deleted)} contas excluídas do grupo!"
+            )
+        else:
+            await sent.edit_text("Não há contas excluídas no grupo!")
+    else:
+        await m.reply_text("Você não é um(a) administrador(a)")
