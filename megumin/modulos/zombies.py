7fdd24c92c8d89150d42f3af838e3331e77d978d
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-03-06 14:30:17 -0300

    Update zombies.py

--- megumin/modulos/zombies.py
+++ megumin/modulos/zombies.py
@@ -32,7 +32,7 @@ async def cleanup(c: megux, m: Message):
                     return
         if deleted:
             await sent.edit_text(
-                f"Eu removi {len(deleted)} contas excluídas do grupo {m.chat.title}!"
+                f"Eu removi **{len(deleted)}** contas excluídas do grupo **{m.chat.title}**!"
             )
         else:
             await sent.edit_text("Não há contas excluídas no aqui!")
