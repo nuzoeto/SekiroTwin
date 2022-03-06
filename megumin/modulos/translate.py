1990d903486d50e75b34c4f448f093836d8c554e
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-03-06 17:52:50 -0300

    Update translate.py

--- megumin/modulos/translate.py
+++ megumin/modulos/translate.py
@@ -81,9 +81,10 @@ async def translate(c: megux, m: Message):
     text = trres.text
 
     res = html.escape(text)
-    await sent.edit_text("<b>Idioma:</b> {from_lang} -> {to_lang}\n<b>Tradução:</b> <code>{translation}</code>").format(
+    await sent.edit_text("<b>Idioma:</b> {from_lang} -> {to_lang}\n<b>Tradução:</b> <code>{translation}</code>".format(
             from_lang=trres.lang, to_lang=langs["targetlang"], translation=res
         )
+    )
 
 
 @megux.on_inline_query(filters.regex(r"^tr"))
