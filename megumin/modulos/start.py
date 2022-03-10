453b1453b3833ebfcef9f5b7ec50f6400b8f2d76
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-03-10 20:25:51 -0300

    Update start.py

--- megumin/modulos/start.py
+++ megumin/modulos/start.py
@@ -74,7 +74,7 @@ async def start_(_, message: Message):
 ╔════「 Sobre  WhiterKang 」
 ╠ Versão : `{version.__megumin_version__}`
 ╠ Uptime : `{time_formatter(time.time() - START_TIME)}`
-╠ System : {uname.system}
+╠ System : `{uname.system}`
 ╠ Cpu : `{psutil.cpu_percent(interval=1)}%`
 ╠ Python : `{version.__python_version__}`
 ╠ Pyrogram : `{version.__pyro_version__}`
