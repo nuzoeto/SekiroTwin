3810d2e90a3d788bc12e2fe9f154ff6baa825a95
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-03-12 14:19:25 -0300

    Update promoter.py

--- megumin/modulos/admin/promoter.py
+++ megumin/modulos/admin/promoter.py
@@ -60,9 +60,15 @@ async def _promote_user(_, message: Message):
             can_invite_users=True,
             can_pin_messages=True,
         )
-        if args:
+        if not args:
             await asyncio.sleep(2)
-        await sent.edit("**Promovido(a)!**")
+        await sent.edit("<b>Promovido(a)!</b>")
+    except Exception as e_f:
+        await sent.edit(f"`Algo deu errado! 🤔`\n\n**ERROR:** `{e_f}`")
+        else:
+            await asyncio.sleep(2)
+        await megux.set_administrator_title(chat_id, user_id, args)
+        await sent.edit("<b>Promovido(a)!</b>")
     except Exception as e_f:
         await sent.edit(f"`Algo deu errado! 🤔`\n\n**ERROR:** `{e_f}`")
 
@@ -109,50 +115,6 @@ async def _demote_user(_, message: Message):
             can_pin_messages=False,
             can_manage_chat=False,
         )
-        await sent.edit("**Rebaixado!**")
-    except Exception as e_f:
-        await sent.edit(f"`Algo deu errado! 🤔`\n\n**ERROR:** `{e_f}`")
-
-
-@megux.on_message(filters.command("title", prefixes=["/", "!"]))
-async def _promote_user(_, message: Message):
-    chat_id = message.chat.id
-    if not await check_rights(chat_id, message.from_user.id, "can_promote_members"):
-        await message.reply("Você não tem as seguintes permissões: **Change can promote members**")
-        return
-    replied = message.reply_to_message
-    args = message.text.split(maxsplit=1)[1]
-    if replied:
-        id_ = replied.from_user.id
-    elif len(message.text) > 8:
-        _, id_ = message.text.split(maxsplit=1)
-    else:
-        await message.reply("`Nenhum User_id válido ou mensagem especificada.`")
-        return
-    try:
-        user_id = (await megux.get_users(id_)).id
-    except (UsernameInvalid, PeerIdInvalid, UserIdInvalid):
-        await message.reply(
-            "`User_id ou nome de usuário inválido, tente novamente com informações válidas ⚠`"
-        )
-        return
-    if await is_self(user_id):
-        return
-    if is_admin(chat_id, user_id):
-        await message.reply("Este usuário já é um administrador ele não precisa ser promovido.")
-        return
-    if not await check_bot_rights(chat_id, "can_promote_members"):
-        await message.reply("Eu não tenho as seguintes permissões: **Change can promote members**")
-        await sed_sticker(message)
-        return
-    sent = await message.reply("`Alterando título de administrador...`")
-    try:
-        if not args:
-            await sent.edit("`Quer que eu adivinhe? Por favor forneça um título para que eu possa alterar`")
-            return
-        else:
-            await asyncio.sleep(2)
-        await megux.set_administrator_title(chat_id, user_id, args)
-        await sent.edit("<b>Novo título de administrador definido com sucesso!!</b>\n<b>Titulo:</b> <code>{args}</code>")
+        await sent.edit("<b>Rebaixado!</b>")
     except Exception as e_f:
         await sent.edit(f"`Algo deu errado! 🤔`\n\n**ERROR:** `{e_f}`")
