d99b144ef575874d7c8bae8a9d622c60f762d175
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-03-15 18:15:03 -0300

    Update youtube.py

--- megumin/modulos/youtube.py
+++ megumin/modulos/youtube.py
@@ -92,6 +92,7 @@ async def song(client: megux, message: Message):
 **Título:** __[{result[0]['title']}]({link})__
 **Duração:** __{duration}__
 **Views:** __{result[0]['viewCount']["text"]}__
+**Canal:** __{result[0]['channel']}__
 """
             try:
                 await msg.reply_audio(
@@ -136,7 +137,7 @@ async def video_(client: megux, message: Message):
         print(str(e))
     else:
         await msg.edit("📦 __Enviando...__")
-        caption = f"**Título ➠** __[{result[0]['title']}]({link})__\n**Duração ➠** __{duration}__\n**Canal ➠** __{result[0]['channel']}__"
+        caption = f"**Título ➠** __[{result[0]['title']}]({link})__\n**Duração ➠** __{duration}__\n**Views ➠** __{result[0]['viewCount']["text"]}__\n**Canal ➠** __{result[0]['channel']}__"
         try:
             await msg.reply_video(
                 video=f"./megumin/xcache/{filename}",
