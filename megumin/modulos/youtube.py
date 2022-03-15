127885e0fc6d7c8e072877698f217499a4df5d15
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-03-15 18:21:10 -0300

    Update youtube.py

--- megumin/modulos/youtube.py
+++ megumin/modulos/youtube.py
@@ -92,7 +92,6 @@ async def song(client: megux, message: Message):
 **Título:** __[{result[0]['title']}]({link})__
 **Duração:** __{duration}__
 **Views:** __{result[0]['viewCount']["text"]}__
-**Canal:** __{result[0]['channel']}__
 """
             try:
                 await msg.reply_audio(
