c3f1234a64a195a0cb7657217707cccf4dbcc03f
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-03-19 16:37:31 -0300

    Update promoter.py

--- megumin/modulos/admin/promoter.py
+++ megumin/modulos/admin/promoter.py
@@ -112,3 +112,29 @@ async def _demote_user(_, message: Message):
         await sent.edit("<b>Rebaixado!</b>")
     except Exception as e_f:
         await sent.edit(f"`Algo deu errado! 🤔`\n\n**ERROR:** `{e_f}`")
+
+
+@megux.on_message(filters.command(["title", "settitle"], prefixes["/", "!"]))
+async def set_user_title(_, message: Message):
+    if not await check_rights(chat_id, message.from_user.id, "can_promote_members"):
+        await message.reply("Você não tem as seguintes permissões: **Change can promote members**")
+        return
+    if not message.reply_to_message:
+        return await message.reply_text(
+            "Reply to user's message to set his admin title"
+        )
+    if not message.reply_to_message.from_user:
+        return await message.reply_text(
+            "I can't change admin title of an unknown entity"
+        )
+    chat_id = message.chat.id
+    from_user = message.reply_to_message.from_user
+    if len(message.command) < 2:
+        return await message.reply_text(
+            "**Usage:**\n/settitle NEW ADMINISTRATOR TITLE"
+        )
+    title = message.text.split(None, 1)[1]
+    await app.set_administrator_title(chat_id, from_user.id, title)
+    await message.reply_text(
+        f"Successfully Changed {from_user.mention}'s Admin Title To {title}"
+    )
