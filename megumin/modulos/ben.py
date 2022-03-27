bae0383567e5feef6a7c620a85a9d91dedc26aea
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-03-27 14:27:55 -0300

    Update ben.py

--- megumin/modulos/ben.py
+++ megumin/modulos/ben.py
@@ -12,7 +12,9 @@ from megumin import megux
 
 @megux.on_message(filters.command(["bun"], prefixes=["/", "!"]))
 async def get_stats(_, message):
-
-  sent = await message.reply("```Banindo usuario...```")
-  await asyncio.sleep(1.1)
-  await sent.edit(f"Usuário <a href='tg://user?id={message.reply_to_message.from_user.id}'>{message.reply_to_message.from_user.first_name}</a>  foi banido por <a href='tg://user?id={message.from_user.id}'>{message.from_user.first_name}</a> no chat: **{message.chat.title}**")
+    if not message.reply_to_message:
+    await m.reply("Você precisará responder a alguém para ser banido com fakeban.")
+    else:
+    sent = await message.reply("```Banindo usuario...```")
+    await asyncio.sleep(1.1)
+    await sent.edit(f"Usuário <a href='tg://user?id={message.reply_to_message.from_user.id}'>{message.reply_to_message.from_user.first_name}</a> foi banido por <a href='tg://user?id={message.from_user.id}'>{message.from_user.first_name}</a> no chat: **{message.chat.title}**")
