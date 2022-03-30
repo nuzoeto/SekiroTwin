e0b6ae4b0afc9acce8d991ea49e430f39a659e5e
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-03-30 20:21:09 -0300

    Create report.py

---
+++ megumin/modulos/report.py
@@ -0,0 +1,25 @@
+from pyrogram import filters
+from pyrogram.types import Message
+
+from megumin import megux
+
+
+@megux.on_message(
+    (filters.command("report", prefixes=["/", "!"]) | filters.regex("^@admin"))
+    & filters.group
+    & filters.reply
+)
+async def reportadmins(c: megux, m: Message, strings):
+    if m.reply_to_message.from_user:
+        check_admin = await m.chat.get_member(m.reply_to_message.from_user.id)
+        if check_admin.status not in admin_status:
+            mention = ""
+            async for i in m.chat.iter_members(filter="administrators"):
+                if not (i.user.is_deleted or i.is_anonymous or i.user.is_bot):
+                    mention += f"<a href='tg://user?id={i.user.id}'>\u2063</a>"
+            await m.reply_to_message.reply_text(
+                "{admins_list}{reported_user} reportado para os administradores.".format(
+                    admins_list=mention,
+                    reported_user=m.reply_to_message.from_user.mention(),
+                ),
+            )
