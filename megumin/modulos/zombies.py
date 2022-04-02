a699a898d2844eb97217e37f431324db8f3497dc
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-04-02 08:48:50 -0300

    Update zombies.py

--- megumin/modulos/zombies.py
+++ megumin/modulos/zombies.py
@@ -24,7 +24,7 @@ async def cleanup(c: megux, m: Message):
     if m.chat.type == "private":
         await m.reply_text("Este comando é para ser usado em grupos!")
         return
-    if not await admin_check(chat_id, m.from_user.id)
+    if not await admin_check(chat_id, m.from_user.id):
         await m.reply_text("Você não é um(a) administrador(a)!")
         return
     if await check_rights(chat_id, m.from_user.id, "can_restrict_members"):
