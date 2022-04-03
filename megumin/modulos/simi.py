a264abb7cab9c52f45c7ff46442e5285a891ccf3
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-04-03 14:46:48 -0300

    Create simi.py

---
+++ megumin/modulos/simi.py
@@ -0,0 +1,14 @@
+import requests 
+
+from pyrogram import filters 
+from pyrogram.types import Message
+
+from megumin import megux 
+
+
+@megux.on_message(filters.command("simi", prefixes=["/", "!"]))
+async def chatbot_(c: megux, m: Message):
+    text_ = m.text.split(maxsplit=1)[1]
+    API = f"https://api.simsimi.net/v2/?text={text_}&lc=pt&cf=false"
+    r = requests.get(API).json()
+    await m.reply(r)
