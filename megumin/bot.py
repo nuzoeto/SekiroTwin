c60fc9378cbc8f449761e130bf4f16d87c429eae
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-04-03 12:13:58 -0300

    Update bot.py

--- megumin/bot.py
+++ megumin/bot.py
@@ -27,7 +27,7 @@ class MeguBot(Client):
         await super().start()
         self.me = await self.get_me()
         text_ = f"#Whiter #Logs\n\n__WhiterKang está trabalhando agora.__\n\n**Versão:** {version.__megumin_version__}\n**System:** {self.system_version}\n**Python:** {version.__python_version__}\n**Pyrogram:** {version.__pyro_version__}"
-        await self.send_mesaage(chat_id=GP_LOGS, text=text_) 
+        await self.send_message(chat_id=GP_LOGS, text=text_) 
         print("WhiterKang esta acordando...")
 
     async def stop(self):
