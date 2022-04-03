7de95095ab03342cf861e9239d02067f7a5aabef
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-04-03 12:06:49 -0300

    Update bot.py

--- megumin/bot.py
+++ megumin/bot.py
@@ -8,6 +8,10 @@ import os
 
 START_TIME = time.time()
 
+from megumin import version
+
+GP_LOGS = -1001556292785
+
 class MeguBot(Client):
     def __init__(self):
         kwargs = {
@@ -21,6 +25,9 @@ class MeguBot(Client):
 
     async def start(self):
         await super().start()
+        self.me = await self.get_me()
+        text_ = f"#Whiter #Logs\n\n__WhiterKang está trabalhando agora.__\n\n**Versão:** {version.__megumin_version__}\n**System:** {self.system_version}\n**Python:** {version.__python_version__}\n**Pyrogram:** {version.__pyro_version__}"
+        await self.send_mesaage(chat_id=GP_LOGS, text=text_) 
         print("WhiterKang esta acordando...")
 
     async def stop(self):
