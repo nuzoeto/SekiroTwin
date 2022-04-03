321fdfa8ae80209963a7c27805fc40aaf23f8e7e
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-04-03 12:55:08 -0300

    Update restart.py

--- megumin/modulos/restart.py
+++ megumin/modulos/restart.py
@@ -1,17 +1,19 @@
 import sys
 import os
-import random
 import re
-import requests
-import wget
-import datetime
 import signal
 import asyncio 
+import traceback 
+import subprocess
+import io
+
 
 from pyrogram import filters
 from pyrogram.types import Message
+from datetime import datetime 
 
 from megumin import megux
+from megumin.utils import is_dev
 
 @megux.on_message(filters.command("restart") & filters.user(1715384854))
 async def broadcast(c: megux, m: Message):
@@ -25,3 +27,20 @@ async def broadcast(c: megux, m: Message):
 async def shutdown(c: megux, m: Message):
     await m.reply_text("**WhiterKang foi desligado!**")
     os.kill(os.getpid(), signal.SIGINT)
+
+
+@megux.on_message(filters.command(["up", "update"]))
+async def restart_(_, message: Message):
+    user_id = message.from_user.id
+    if not is_dev(user_id):
+        return
+    process = subprocess.Popen(["git", "pull"], stdout=subprocess.PIPE)
+    output = process.communicate()[0]
+    kek = await message.reply(f"`{output}`")
+    await asyncio.sleep(2)
+    await kek.edit("**WhiterKang foi atualizado!**")
+    await asyncio.sleep(3)
+    await kek.edit("`Reiniciando...`")
+    await asyncio.sleep(2)
+    await kek.edit("**WhiterKang foi Reiniciado com Sucesso!**")
+    os.execv(sys.executable, [sys.executable, "-m", "megumin"])
