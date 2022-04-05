8e7daa9c11fc5906f9a46cd98fef24e5a5b42179
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-04-04 21:10:41 -0300

    Update stats.py

--- megumin/modulos/stats.py
+++ megumin/modulos/stats.py
@@ -2,6 +2,10 @@ import os
 from pyrogram import filters
 
 from megumin import megux
+from megumin.utils import get_collection
+
+USERS = get_collection("USERS")
+GROUPS = get_collection("GROUPS")
 
 @megux.on_message(filters.command(["neofetch", "sys", "stats"]))
 async def get_stats(_, message):
@@ -17,3 +21,13 @@ async def get_stats(_, message):
 {neofetch}
 ```''')
     await message.reply_text(stats)
+
+
+@megux.on_message(filters.command(["status"]))
+async def status_(_, m: Message):
+    user_id = m.from_user.id
+    if not is_dev(user_id):
+        return
+    glist = await GROUPS.estimated_document_count()
+    ulist = await USERS.estimated_document_count()
+    await m.reply(f"**♬🎷【 Bot Status 】●♪**\n\n**Users**: __{ulist}__\n**Groups**: __{glist}__")  
