26e9214a7c7add8369e2943006c7b1671e596ea5
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-04-05 19:29:47 -0300

    Update whois.py

--- megumin/modulos/whois.py
+++ megumin/modulos/whois.py
@@ -98,36 +98,3 @@ async def whois(client, message):
         )
 
 
-@megux.on_inline_query(filters.regex(r"^info"))
-async def info_inline(c: Client, q: InlineQuery, strings):
-    try:
-        if q.query == "info":
-            user = q.from_user
-        elif q.query.lower().split(None, 1)[1]:
-            txt = q.query.lower().split(None, 1)[1]
-            user = await c.get_users(txt)
-    except (PeerIdInvalid, UsernameInvalid, UserIdInvalid):
-        await q.answer(
-            [
-                InlineQueryResultArticle(
-                    title="Usuário não encontrado.",
-                    input_message_content=InputTextMessageContent(
-                        "Usuário não encontrado."
-                    ),
-                )
-            ]
-        )
-    await q.answer(
-        [
-            InlineQueryResultArticle(
-                title="Clique aqui para obter informações do usuário.",
-                input_message_content=InputTextMessageContent(
-                    "Nome de usuário: {usernameformat}\nID: {useridformat}\nDC: {userdcformat}\nLink do usuário: {usermentionformat}".format(
-                        usernameformat=user.username,
-                        useridformat=user.id,
-                        userdcformat=user.dc_id,
-                        usermentionformat=user.mention(),
-                    ),
-                ),
-            )
-        ]
