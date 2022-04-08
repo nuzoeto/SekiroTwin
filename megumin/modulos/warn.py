c769a703ddf2dfb217f6690c3cd035061b46d5bd
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-04-08 11:49:02 -0300

    Update warn.py

--- megumin/modulos/warn.py
+++ megumin/modulos/warn.py
@@ -16,9 +16,25 @@ from megumin.utils import get_collection
 @megux.on_message(filters.command(["warm"], prefixes=["/", "!"]))
 async def warm_(_, message):
     ids = (message.reply_to_message.from_user.id)
-    WARMS = get_collection(f"WARM {ids}")
+    WARMS = get_collection(f"WARMS {ids}")
     name_user = (message.reply_to_message.from_user.mention())
     await asyncio.gather(WARMS.insert_one({"id_": ids, "title": name_user}))
+    G = await WARMS.estimated_document_count()
+    keyboard = InlineKeyboardMarkup(
+            [
+                [InlineKeyboardButton("Remover Advertencia",
+              callback_data="remove_warm_")]
+            ]
+        )
+    await message.reply(f"Usuario {name_user} tem {G}/3 Advertências tenha cuidado!\nReason: <code>Xingando o bot</code>", reply_markup=(keyboard))
+
+
+@megux.on_message(filters.command(["unwarm"], prefixes=["/", "!"]))
+async def warm_(_, message):
+    ids = (message.reply_to_message.from_user.id)
+    WARMS = get_collection(f"WARMS {ids}")
+    name_user = (message.reply_to_message.from_user.mention())
+    await asyncio.gather(WARMS.delete_one({"id_": ids, "title": name_user}))
     G = await WARMS.estimated_document_count()
     keyboard = InlineKeyboardMarkup(
             [
