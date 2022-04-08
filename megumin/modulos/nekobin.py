631aa310dd3b6e6f38a093d420e9e3d3f60862ff
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-04-08 13:09:37 -0300

    Create nekobin.py

---
+++ megumin/modulos/nekobin.py
@@ -0,0 +1,27 @@
+import httpx
+
+from pyrogram import filters
+from pyrogram.types import Message
+
+from megumin import megux 
+
+http = httpx.AsyncClient()
+
+
+@megux.on_message(filters.command("paste", prefixes=["/", "!"]))
+async def nekobin(c: Client, m: Message, strings):
+    if m.reply_to_message:
+        if m.reply_to_message.document:
+            tfile = m.reply_to_message
+            to_file = await tfile.download()
+            with open(to_file, "rb") as fd:
+                mean = fd.read().decode("UTF-8")
+        if m.reply_to_message.text:
+            mean = m.reply_to_message.text
+
+        url = "https://nekobin.com/api/documents"
+        r = await http.post(url, json={"content": mean})
+        url = f"https://nekobin.com/{r.json()['result']['key']}"
+        await m.reply_text("<b>Nekobin [URL]</b>(url)", disable_web_page_preview=True)
+    else:
+        await m.reply_text("Por favor, responda a um texto ou documento para colar o conteúdo.")
