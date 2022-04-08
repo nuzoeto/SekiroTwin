3a34f56d32b3a9512654e875dc526180f99b8348
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-04-08 11:16:59 -0300

    Create warn.py

---
+++ megumin/modulos/warn.py
@@ -0,0 +1,37 @@
+import os
+import asyncio 
+
+from pyrogram import filters 
+from pyrogram.types import (CallbackQuery, 
+InlineKeyboardButton, 
+InlineKeyboardMarkup,
+Message, 
+)
+
+
+from megumin import megux 
+from megumin.utils import get_collection
+
+
+m = Message
+
+ids = (m.reply_to_message.from_user.id)
+WARMS = get_collection(f"WARM {ids}")
+name_user = (message.reply_to_message.from_user.mention())
+
+
+@megux.on_message(filters.command(["warm"], prefixes=["/", "!"]))
+async def get_stats(_, message):
+    await asyncio.gather(WARMS.insert_one({"id_": ids, "title": name_user}))
+    G = await WARMS.estimated_document_count()
+    keyboard = InlineKeyboardMarkup(
+            [
+                [InlineKeyboardButton("Remover Advertencia",
+              callback_data="remove_warm_")]
+            ]
+        )
+    await m.reply(f"Usuario {name_user} tem {G}/3 Advertências tenha cuidado!\nReason: <code>Xingando o bot</code>", reply_markup=(keyboard))
+    
+          
+
+
