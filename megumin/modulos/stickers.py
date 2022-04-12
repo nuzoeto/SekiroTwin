606b22fd7ea328c2761c79764c9a55cc25a08552
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-04-12 17:05:05 -0300

    Update stickers.py

--- megumin/modulos/stickers.py
+++ megumin/modulos/stickers.py
@@ -89,6 +89,8 @@ async def getstickerid(c: megux, m: Message):
 
 @megux.on_message(filters.command("kang", prefixes=["/", "!"]))
 async def kang_sticker(c: megux, m: Message):
+    if m.chat.id == -1001471874620:
+       return
     prog_msg = await m.reply_text("Roubando o sticker...")
     user = await c.get_me()
     bot_username = user.username
@@ -101,7 +103,7 @@ async def kang_sticker(c: megux, m: Message):
     reply = m.reply_to_message
     user = await c.resolve_peer(m.from_user.username or m.from_user.id)
 
-    if reply and reply.media:
+    elif reply and reply.media:
         if reply.photo:
             resize = True
         elif reply.animation:
