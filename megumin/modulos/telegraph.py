70d9dfdbec7440881d2d9a3788d0e96933210a67
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-04-15 17:09:26 -0300

    Update telegraph.py

--- megumin/modulos/telegraph.py
+++ megumin/modulos/telegraph.py
@@ -42,7 +42,7 @@ async def telegraph_(megux, message: Message):
     except Exception as document:
         await msg.edit(document)
     else:
-        link = f"<b>[Aqui, seu link telegraph!](https://telegra.ph{response[0]}</b>)"
+        link = f"<b>[Aqui, seu link telegraph!](https://telegra.ph{response[0]})</b>"
         await msg.edit(link, disable_web_page_preview=True)
     finally:
         os.remove(download_location)
