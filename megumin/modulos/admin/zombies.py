97481f1c21ba22fb6bb24dd84a405aa38cd44060
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-04-18 12:22:31 -0300

    Update zombies.py

--- megumin/modulos/admin/zombies.py
+++ megumin/modulos/admin/zombies.py
@@ -21,7 +21,7 @@ from megumin.utils import (
 @megux.on_message(filters.command("cleanup", prefixes=["/", "!"]))
 async def cleanup(c: megux, m: Message):
     if m.chat.type == "private":
-        return await m.reply_text("Admin.err_private")
+        return await m.reply_text("Esse comando não pode ser usado em um grupo privado, use esse comando em um grupo que você administra.")
     else:
         bot = await c.get_chat_member(chat_id=m.chat.id, user_id=(await c.get_me()).id)
         member = await c.get_chat_member(chat_id=m.chat.id, user_id=m.from_user.id)
@@ -29,17 +29,17 @@ async def cleanup(c: megux, m: Message):
             if bot.status in ["administrator"]:
                 pass
             else:
-                return await m.reply_text("Admin.botnotadmin")
+                return await m.reply_text("Eu não sou um administrador do grupo!")
         else:
-            return await m.reply_text("Admin.noadmin")
+            return await m.reply_text("Você não é um administrador do grupo")
     deleted_users = []
-    sent = await m.reply_text("Admin.cleanup_start")
+    sent = await m.reply_text("Removendo contas excluídas...")
     async for a in c.iter_chat_members(chat_id=m.chat.id, filter="all"):
         if a.user.is_deleted:
             try:
                 await c.ban_chat_member(m.chat.id, a.user.id)
                 deleted_users.append(a)
-                await sent.edit_text("Admin.cleanup_start_d {}").format(
+                await sent.edit_text("Contas excluídas: {}").format(
                     {len(deleted_users)}
                 )
             except BadRequest:
@@ -47,4 +47,4 @@ async def cleanup(c: megux, m: Message):
             except Forbidden as e:
                 return await m.reply_text(f"<b>Erro:</b> {e}")
         else:
-            await sent.edit_text("Admin.cleanup_no_deleted")
+            await sent.edit_text("Não há contas excluídas no grupo.")
