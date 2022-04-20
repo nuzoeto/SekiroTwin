2bd29d164684293a9541df1dda357f5fdbbf05a5
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-04-19 21:57:54 -0300

    Update paste.py

--- megumin/modulos/paste.py
+++ megumin/modulos/paste.py
@@ -28,21 +28,3 @@ async def nekobin(c: megux, m: Message):
         await m.reply_text("Por favor, responda a um texto ou documento para colar o conteúdo.")
 
 
-@megux.on_message(filters.command("hastebin", prefixes=["/", "!"]))
-async def hastebin(c: megux, m: Message):
-    if m.reply_to_message:
-        if m.reply_to_message.document:
-            tfile = m.reply_to_message
-            sm = await m.reply("<i>Processando...</i>")
-            to_file = await tfile.download()
-            with open(to_file, "rb") as fd:
-                mean = fd.read().decode("UTF-8")
-        if m.reply_to_message.text:
-            mean = m.reply_to_message.text
-
-        url = "https://hastebin.com/documents"
-        r = await http.post(url, data=mean.encode("UTF-8"))
-        url = f"https://hastebin.com/{r.json()['key']}"
-        await sm.edit(f"<b>Hastebin [URL]({url})</b>", disable_web_page_preview=True)
-    else:
-        await m.reply_text("Por favor, responda a um texto ou documento para colar o conteúdo.")
