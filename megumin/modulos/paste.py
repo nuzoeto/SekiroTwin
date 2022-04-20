abeda0c72ca5ada17569e2deae8ce065a50b51a8
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-04-19 22:00:21 -0300

    Update paste.py

--- megumin/modulos/paste.py
+++ megumin/modulos/paste.py
@@ -13,7 +13,6 @@ async def nekobin(c: megux, m: Message):
     if m.reply_to_message:
         if m.reply_to_message.document:
             tfile = m.reply_to_message
-            sm = await m.reply_text("<i>Processando...</i>")
             to_file = await tfile.download()
             with open(to_file, "rb") as fd:
                 mean = fd.read().decode("UTF-8")
@@ -23,7 +22,7 @@ async def nekobin(c: megux, m: Message):
         url = "https://nekobin.com/api/documents"
         r = await http.post(url, json={"content": mean})
         url = f"https://nekobin.com/{r.json()['result']['key']}"
-        await sm.edit_text(f"<b>Nekobin [URL]({url})</b>", disable_web_page_preview=True)
+        await m.reply_text(f"<b>Nekobin [URL]({url})</b>", disable_web_page_preview=True)
     else:
         await m.reply_text("Por favor, responda a um texto ou documento para colar o conteúdo.")
 
