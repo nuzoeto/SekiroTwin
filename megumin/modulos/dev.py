c74f9db292a14a1fb9f691eea012c84eda4985a7
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-04-22 10:37:34 -0300

    Update dev.py

--- megumin/modulos/dev.py
+++ megumin/modulos/dev.py
@@ -98,3 +98,76 @@ async def aexec(code, client, message):
         + "".join(f"\n {l_}" for l_ in code.split("\n"))
     )
     return await locals()["__aexec"](client, message)
+
+
+megux.on_message(filters.command(["term", "sh"], prefixes=["/", "!"]))
+async def terminal(client: megux, message: Message):
+    user_id = message.from_user.id
+    if not is_dev(user_id):
+        return
+    if len(message.text.split()) == 1:
+        await message.reply_text("Usage: `/term echo owo`")
+        return
+    args = message.text.split(None, 1)
+    teks = args[1]
+    if "\n" in teks:
+        code = teks.split("\n")
+        output = ""
+        for x in code:
+            shell = re.split(""" (?=(?:[^'"]|'[^']*'|"[^"]*")*$)""", x)
+            try:
+                process = subprocess.Popen(
+                    shell, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+                )
+            except Exception as err:
+                print(err)
+                await message.reply_text(
+                    """
+**Error:**
+```{}```
+""".format(
+                        err
+                    ),
+                    parse_mode="markdown",
+                )
+            output += "**{}**\n".format(code)
+            output += process.stdout.read()[:-1].decode("utf-8")
+            output += "\n"
+    else:
+        shell = re.split(""" (?=(?:[^'"]|'[^']*'|"[^"]*")*$)""", teks)
+        for a in range(len(shell)):
+            shell[a] = shell[a].replace('"', "")
+        try:
+            process = subprocess.Popen(
+                shell, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+            )
+        except Exception as err:
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            errors = traceback.format_exception(
+                etype=exc_type, value=exc_obj, tb=exc_tb
+            )
+            await message.reply_text(
+                """**Error:**\n```{}```""".format("".join(errors)),
+                parse_mode="markdown",
+            )
+            return
+        output = process.stdout.read()[:-1].decode("utf-8")
+    if str(output) == "\n":
+        output = None
+    if output:
+        if len(output) > 4096:
+            filename = "output.txt"
+            with open(filename, "w+") as file:
+                file.write(output)
+            await client.send_document(
+                message.chat.id,
+                filename,
+                reply_to_message_id=message.message_id,
+                caption="`Output file`",
+            )
+            os.remove(filename)
+            return
+        await message.reply_text(f"**Output:**\n```{output}```", parse_mode="markdown")
+    else:
+        await message.reply_text("**Output:**\n`No Output`")
+    return await locals()["__aexec"](client, message)
