e9c71e394dd6f93d1a3e969c68c72232f5204e68
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-04-23 10:37:53 -0300

    Update youtube.py

--- megumin/modulos/youtube.py
+++ megumin/modulos/youtube.py
@@ -28,41 +28,12 @@ def get_yt_video_id(url: str):
     if match:
         return match.group(1)
 
-
-async def get_link(query):
-    vid_id = get_yt_video_id(query)
-    link = f"{BASE_YT_URL}{vid_id}"
-    if vid_id is None:
-        try:
-            res_ = SearchVideos(query, offset=1, mode="json", max_results=1)
-            link = json.loads(res_.result())["search_result"][0]["link"]
-            id_ = link = json.loads(res_.result())["search_result"][0]["id"]
-            return link, id_
-        except Exception as e:
-            return e
-    else:
-        return link, vid_id
-
-# yt-dl args - extract video info
-async def extract_inf(link, opts_):
-    with YoutubeDL(opts_) as ydl:
-        infoo = ydl.extract_info(link, False)
-        ydl.process_info(infoo)
-        duration_ = infoo["duration"]
-        title_ = infoo["title"]
-        channel_ = infoo["channel"]
-        views_ = infoo["view_count"]
-        capt_ = f"<a href={link}><b>{title_}</b></a>\n❯ Duração: {duration_}\n❯ Views: {views_}\n❯ Canal: {channel_}"
-        return capt_, title_, duration_
-
-
 @megux.on_message(filters.command(["video"], prefixes=["/", "!"]))
 async def vid_(c: megux, message: Message):
-    chat_id = message.chat.id 
     query = " ".join(message.text.split()[1:])
     if not query:
-        return await message.reply_text("`Vou baixar o vento?!`")
-    msg = await message.reply_text("📦 <i>Baixando...</i>")
+        return await message.reply("`Vou baixar o vento?!`", del_in=5)
+    msg = await message.reply("`Aguarde ...`")
     vid_opts = {
         "outtmpl": os.path.join(Config.DOWN_PATH, "%(title)s.%(ext)s"),
         'writethumbnail': False,
@@ -76,21 +47,20 @@ async def vid_(c: megux, message: Message):
         "quiet": True,
     }
     link = await get_link(query)
+    await msg.edit("`Processando o video ...`")
     filename_, capt_, duration_ = extract_inf(link, vid_opts)
-    if int(duration_) > 3600:
-        return await msg.edit("__Esse vídeo é muito longo, a duração máxima é de 1 hora__")
     if filename_ == 0:
         _fpath = ''
         for _path in glob.glob(os.path.join(Config.DOWN_PATH, '*')):
             if not _path.lower().endswith((".jpg", ".png", ".webp")):
                 _fpath = _path
         if not _fpath:
-            return await msg.edit("Não foi possível encontrar o video na pasta!")
-        await msg.edit("📦 <i>Enviando...</i>")
+            return await msg.edit("nothing found !")
+        await msg.delete()
         await message.reply_video(video=Path(_fpath), caption=capt_, duration=duration_)
         os.remove(Path(_fpath))
     else:
-        await message.edit(str(filename_))
+        await message.reply(str(filename_))
 
 
 @megux.on_message(filters.command(["song", "music"], prefixes=["/", "!"]))
@@ -137,3 +107,36 @@ async def song_(c: megux, message: Message):
     await msg.delete()
     os.remove(f"{Config.DOWN_PATH}{title_}.{fid}")
     os.remove(f"{Config.DOWN_PATH}maxresdefault.jpg")
+
+
+async def get_link(query):
+    vid_id = get_yt_video_id(query)
+    link = f"{BASE_YT_URL}{vid_id}"
+    if vid_id is None:
+        try:
+            res_ = SearchVideos(query, offset=1, mode="json", max_results=1)
+            link = json.loads(res_.result())["search_result"][0]["link"]
+            return link
+        except Exception as e:
+            LOGGER.exception(e)
+            return e
+    else:
+        return link
+
+
+def extract_inf(url, _opts):
+    try:
+        x = YoutubeDL(_opts)
+        infoo = x.extract_info(url, False)
+        x.process_info(infoo)
+        duration_ = infoo["duration"]
+        title_ = infoo["title"].replace("/", "_")
+        channel_ = infoo["channel"]
+        views_ = infoo["view_count"]
+        capt_ = f"<a href={url}><b>{title_}</b></a>\n❯ Duração: {duration_}\n❯ Views: {views_}\n❯ Canal: {channel_}"
+        dloader = x.download(url)
+    except Exception as y_e:  # pylint: disable=broad-except
+        LOGGER.exception(y_e)
+        return y_e
+    else:
+        return dloader, capt_, duration_
