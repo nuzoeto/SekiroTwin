df6fd115c96af7ad9bffbb2c2491cd7deb4e2ca1
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-04-25 23:35:02 -0300

    Update config.py

--- megumin/config.py
+++ megumin/config.py
@@ -29,7 +29,6 @@ class Config:
     API_HASH = os.environ.get("API_HASH")
     BOT_TOKEN = os.environ.get("BOT_TOKEN")
     REMOVE_BG_API_KEY = ("Qo4zoSN6oqHF4H9Ur5rWLzyr") #API REMOVE_BG não meche
-    DOWN_PATH = os.environ.get("DOWN_PATH")
     DB_URI = os.environ.get("DATABASE_URL")
     TRIGGER = os.environ.get("TRIGGER", "/ !".split())
     WHITELIST_CHATS = set([])  # chat id aq
