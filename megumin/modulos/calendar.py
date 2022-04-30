0c88dd711377ce2b1ca3748572ec16e7079d56ae
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-04-30 13:25:17 -0300

    Create calendar.py

---
+++ megumin/modulos/calendar.py
@@ -0,0 +1,30 @@
+import calendar
+from datetime import datetime
+
+from pyrogram import filters 
+from pyrogram.types import Message 
+
+from megumin import megux 
+from megumin.utils.decorators import input_str
+
+@megux.on_message(filters.command("cal"))
+async def cal_(_, message: Message):
+    if not input_str(message):
+        msg = await message.reply("__Procurando...__")
+        try:
+            today = datetime.today()
+            input_ = calendar.month(today.year, today.month)
+            await msg.edit(f"```{input_}```")
+        except Exception as e:
+            await msg.edit(e)
+        return
+    if "|" not in input_str(message):
+        await message.err("both year and month required!")
+        return
+    msg = await message.reply("__Procurando...__")
+    year, month = input_str(message.split("|", maxsplit=1))
+    try:
+        input_ = calendar.month(int(year.strip()), int(month.strip()))
+        await msg.edit(f"```{input_}```")
+    except Exception as e:
+        await msg.edit(e)
