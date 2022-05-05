fb347f8520a564d6bc65f8146ec1ea19d03fb1b4
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-05-05 20:33:03 -0300

    Update mute.py

--- megumin/modulos/admin/mute.py
+++ megumin/modulos/admin/mute.py
@@ -134,7 +134,7 @@ async def _tmute_user(_, message: Message):
         await asyncio.sleep(1)
         await sent.edit(
             f"{mention} está silenciado(mutado) por <b>{time_val}</b> em <b>{message.chat.title}</b>\n"
-            f"Motivo: `{reason or None}`"
+            f"<b>Motivo</b>: `{reason or None}`"
         )
     except Exception as e_f:  # pylint: disable=broad-except
         await sent.edit(f"`Algo deu errado 🤔`\n\n**ERROR**: `{e_f}`")
