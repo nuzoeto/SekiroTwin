5c1efc3005d77594a18ea4bd9cd2bb781f430450
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-05-08 14:17:56 -0300

    Update bans.py

--- megumin/modulos/admin/bans.py
+++ megumin/modulos/admin/bans.py
@@ -163,58 +163,6 @@ async def _kick_user(_, message: Message):
         await sent.edit(f"`Algo deu errado! 🤔`\n\n**ERROR:** `{e_f}`")
 
 
-@megux.on_message(filters.command("skick", prefixes=["/", "!"]))
-async def _kick_user(_, message: Message):
-    chat_id = message.chat.id
-    if not await check_rights(chat_id, message.from_user.id, "can_restrict_members"):
-        await message.reply("Você não tem as seguintes permissões: **Can restrict members**")
-        return
-    cmd = len(message.text)
-    replied = message.reply_to_message
-    reason = ""
-    if replied:
-        id_ = replied.from_user.id
-        if cmd > 5:
-            _, reason = message.text.split(maxsplit=1)
-    elif cmd > 5:
-        _, args = message.text.split(maxsplit=1)
-        if " " in args:
-            id_, reason = args.split(" ", maxsplit=1)
-        else:
-            id_ = args
-    else:
-        await message.reply("`Nenhum user_id válido ou mensagem especificada.`")
-        return
-    try:
-        user = await megux.get_users(id_)
-        user_id = user.id
-        mention = user.mention
-    except (UsernameInvalid, PeerIdInvalid, UserIdInvalid):
-        await message.reply(
-            "`User_id ou nome de usuário inválido, tente novamente com informações válidas ⚠`"
-        )
-        return
-    if await is_self(user_id):
-        await sed_sticker(message)
-        return
-    if is_dev(user_id):
-        await message.reply("Porque eu iria banir meu desenvolvedor? Isso me parece uma idéia muito idiota.")
-        return
-    if is_admin(chat_id, user_id):
-        await message.reply("Porque eu iria kickar um(a) administrador(a)? Isso me parece uma idéia bem idiota.")
-        return
-    if not await check_bot_rights(chat_id, "can_restrict_members"):
-        await message.reply("Não posso restringir as pessoas aqui! Certifique-se de que sou administrador e de que posso adicionar novos administradores.")
-        await sed_sticker(message)
-        return
-    try:
-        await message.delete()
-        await megux.ban_chat_member(chat_id, user_id, int(time.time() + 60))
-        await megux.unban_chat_member(chat_id, user_id)
-        skicked = await message.reply(f"Eu removi o usuário {mention}\n" f"**Motivo**: `{reason or None}`")
-        await skicked.delete()
-    except Exception as e_f:
-        await sent.edit(f"`Algo deu errado! 🤔`\n\n**ERROR:** `{e_f}`")
 
 
 @megux.on_message(filters.command("kickme", prefixes=["/", "!"]))
