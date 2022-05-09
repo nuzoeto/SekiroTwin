58f2cd899f5dc1c75a4b41f0b02668d697e32b6c
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-05-09 18:23:22 -0300

    Create tiktok.py

---
+++ megumin/modulos/tiktok.py
@@ -0,0 +1,11 @@
+import os
+from wget import download
+import requests 
+
+from pyrogram import filters 
+from pyrogram import.types Message
+
+from megumin import megux, Config 
+
+API = "https://hadi-api.herokuapp.com/api/tiktok";
+
