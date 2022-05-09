73a3f22ddeebb102f855757fd4d6d0942c58451c
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-05-09 17:29:55 -0300

    Update youtube.py

--- megumin/modulos/youtube.py
+++ megumin/modulos/youtube.py
@@ -120,7 +120,7 @@ async def vid_(c: megux, message: Message):
             return await msg.edit("nada encontrado !")
         await msg.edit("📦 <i>Enviando...</i>")
         await c.send_chat_action(message.chat.id, "upload_video")
-        await message.reply_video(video=Path(_fpath), caption=capt_, duration=duration_)
+        await message.reply_video(video=Path(_fpath), width=1280, height=720,  caption=capt_, duration=duration_)
         await msg.delete()
         os.remove(Path(_fpath))
         await asyncio.gather(c.send_log("#Send #YOUTUBE_DL"))
