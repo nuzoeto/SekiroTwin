036f3f7407e09987dc91a79bc6200abd814f179b
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-05-12 22:36:39 -0300

    init /google

--- megumin/modulos/print(1).py
+++ megumin/modulos/print(1).py
@@ -78,3 +78,75 @@ async def cssworker_url(target_url: str):
         return resp.json()
     except HTTPError:
         return None
+
+
+@megux.on_message(filters.command("google", prefixes=["/","!"]))
+async def prints(c: megux, message: Message):
+    msg = message.text
+    the_url = msg.split(" ", 1)
+    wrong = False
+
+    if len(the_url) == 1:
+        if message.reply_to_message:
+            the_url = message.reply_to_message.text
+            if len(the_url) == 1:
+                wrong = True
+            else:
+                the_url = the_url[1]
+        else:
+            wrong = True
+    else:
+        the_url = the_url[1]
+
+    if wrong:
+        await message.reply_text("Uso:</b> <code>/print https://example.com</code> - Tira uma captura de tela do site especificado.")
+        return
+
+    try:
+        sent = await message.reply_text("Obtendo captura de tela...")
+        res_json = await cssworker_url(target_url=f"https://www.google.com/search?q={the_url}&oq={the_url}")
+    except BaseException as e:
+        await message.reply(f"<b>Failed due to:</b> <code>{e}</code>")
+        return
+
+    if res_json:
+        # {"url":"image_url","response_time":"147ms"}
+        image_url = res_json["url"]
+        if image_url:
+            try:
+                await message.reply_photo(image_url)
+                await sent.delete()
+            except BaseException:
+                # if failed to send the message, it's not API's
+                # fault.
+                # most probably there are some other kind of problem,
+                # for example it failed to delete its message.
+                # or the bot doesn't have access to send media in the chat.
+                return
+        else:
+            await message.reply(
+                "Couldn't get url value, most probably API is not accessible."
+            )
+    else:
+        await message.reply("Failed because API is not responding, try again later.")
+
+
+async def cssworker_url(target_url: str):
+    url = "https://htmlcsstoimage.com/demo_run"
+    my_headers = {
+        "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:95.0) Gecko/20100101 Firefox/95.0",
+    }
+
+    data = {
+        "url": target_url,
+        "render_when_ready": False,
+        "viewport_width": 1280,
+        "viewport_height": 720,
+        "device_scale": 1,
+    }
+
+    try:
+        resp = await http.post(url, headers=my_headers, json=data)
+        return resp.json()
+    except HTTPError:
+        return None
