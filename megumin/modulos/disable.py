24fb06cc71ae7ca8fe5e47df420899b8fec0ba07
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-05-14 14:50:44 -0300

    Create disable.py

---
+++ megumin/modulos/disable.py
@@ -0,0 +1,21 @@
+from pyrogram import filters
+from pyrogram.types import Message
+
+from megumin import megux, Config
+from megumin.utils import get_collection
+from megumin.utils.decorators import input_str 
+
+
+DISABLED = get_collection("DISABLED")
+CMDS = [
+    "ping"
+]
+
+
+@megux.on_message(filters.command("disable", Config.TRIGGER))
+async def disble_cmd(_, m: Message):
+    gid = m.chat.id
+    query = input_str(m)
+    dis_cmd = await DISABLED.insert_one({'_id': gid, '_cmd': query})
+    if not query in CMDS:
+        return await m.reply("__Qual comando você deseja desativar?__")
