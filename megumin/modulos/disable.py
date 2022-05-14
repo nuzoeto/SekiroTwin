221d68ecf56302fbf29ff5d74f5b73fcd73fc0f5
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-05-14 16:36:33 -0300

    Update disable.py

--- megumin/modulos/disable.py
+++ megumin/modulos/disable.py
@@ -2,7 +2,7 @@ from pyrogram import filters
 from pyrogram.types import Message
 
 from megumin import megux, Config
-from megumin.utils import get_collection
+from megumin.utils import get_collection, check_rights 
 from megumin.utils.decorators import input_str 
 
 
@@ -17,14 +17,21 @@ CMDS = [
 @megux.on_message(filters.command("disable", Config.TRIGGER))
 async def disble_cmd(_, m: Message):
     gid = m.chat.id
+    check_admin = m.from_user.id  
     query = input_str(m)
-    if not query in CMDS:
-        return await m.reply("__Qual comando você deseja desativar?__")
+    if m.chat.type == "private"
+        return await m.reply("Esse comando é para ser usado em grupos")
     else:
-        found = await DISABLED.find_one({'_id': gid, '_cmd': query})
-        if found:
-            return await m.reply("__Comando já desativado!__")
+        if not query in CMDS:
+            return await m.reply("__Qual comando você deseja desativar?__")
         else:
-            dis_cmd = await DISABLED.insert_one({'_id': gid, '_cmd': query})
-            await m.reply(f"__Comando Agora Desativado!!!__")
+            found = await DISABLED.find_one({'_id': gid, '_cmd': query})
+            if found:
+                return await m.reply("__Comando já desativado!__")
+            else:
+                if await check_rights(chat_id, check_admin, "can_change_info")
+                    dis_cmd = await DISABLED.insert_one({'_id': gid, '_cmd': query})
+                    await m.reply(f"__Comando Agora Desativado!!!__")
+                else:
+                    return await m.reply("Você não tem direitos administrativos suficientes para alterar dados do grupo!")
         
