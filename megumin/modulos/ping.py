1c14b58d67fca35b597035c483b8c2aa6bbe834e
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-05-14 15:17:36 -0300

    Update ping.py

--- megumin/modulos/ping.py
+++ megumin/modulos/ping.py
@@ -32,5 +32,5 @@ async def pingme(_, message: Message):
         end = datetime.now()
         m_s = (end - start).microseconds / 1000
         await sla.edit(f"🏓 **Ping:** ```{m_s} ᴍs``` \n⏱ **Uptime:** ```{time_formatter(time.time() - START_TIME)}```")
-       if off:
-           return
+        if off:
+            return
