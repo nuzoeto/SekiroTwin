2dbaa708cfd68ad2e027ca94e2d876b1d159cc5d
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-05-14 22:37:31 -0300

    Update disable.py

--- megumin/modulos/disable.py
+++ megumin/modulos/disable.py
@@ -6,7 +6,7 @@ from megumin.utils import get_collection, check_rights
 from megumin.utils.decorators import input_str 
 
 
-DISABLED = get_collection("DISABLED")
+DISABLED = get_collection(f"DISABLED {Message.chat.id}")
 CMDS = [
     "ping",
     "print",
@@ -22,7 +22,6 @@ CMDS = [
 
 @megux.on_message(filters.command("disable", Config.TRIGGER))
 async def disble_cmd(_, m: Message):
-    gid = m.chat.id
     chat_id = m.chat.id 
     check_admin = m.from_user.id  
     query = input_str(m)
@@ -32,12 +31,12 @@ async def disble_cmd(_, m: Message):
         if not query in CMDS:
             return await m.reply("__Qual comando você deseja desativar?__")
         else:
-            found = await DISABLED.find_one({'_id': gid, '_cmd': query})
+            found = await DISABLED.find_one({'_cmd': query})
             if found:
                 return await m.reply("__Comando já desativado!__")
             else:
                 if await check_rights(chat_id, check_admin, "can_change_info"):
-                    dis_cmd = await DISABLED.insert_one({'_id': gid, '_cmd': query})
+                    dis_cmd = await DISABLED.insert_one({'_cmd': query})
                     await m.reply(f"__Comando Agora Desativado!!!__")
                 else:
                     return await m.reply("Você não tem direitos administrativos suficientes para alterar dados do grupo!")
@@ -45,8 +44,7 @@ async def disble_cmd(_, m: Message):
 
 @megux.on_message(filters.command("enable", Config.TRIGGER))
 async def enable_cmd(_, m: Message):
-    gid = m.chat.id
-    chat_id = m.chat.id 
+    chat_id = m.chat.id
     check_admin = m.from_user.id  
     query = input_str(m)
     if m.chat.type == "private":
@@ -55,10 +53,10 @@ async def enable_cmd(_, m: Message):
         if not query in CMDS:
             return await m.reply("__Qual comando você deseja ativar?__")
         else:
-            found = await DISABLED.find_one({'_id': gid, '_cmd': query})
+            found = await DISABLED.find_one({'_cmd': query})
             if found:
                 if await check_rights(chat_id, check_admin, "can_change_info"):
-                    dis_cmd = await DISABLED.delete_one({'_id': gid, '_cmd': query})
+                    dis_cmd = await DISABLED.delete_one({'_cmd': query})
                     await m.reply(f"__Comando Agora Ativado!!!__")
                 else:
                     return await m.reply("Você não tem direitos administrativos suficientes para alterar dados do grupo!")
