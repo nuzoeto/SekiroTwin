e8e7538920369b0283d4f03dd7940167807756b5
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-05-18 16:52:02 -0300

    Create warn.py

---
+++ megumin/modulos/warn.py
@@ -0,0 +1,24 @@
+from pyrogram import filters 
+from pyrogram.types import Message 
+
+from megumin import megux 
+from megumin.utils import get_collection 
+
+
+ACTION = [
+    "ban",
+    "mute",
+    "kick",
+]
+
+NUMBER = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+]
+
+
