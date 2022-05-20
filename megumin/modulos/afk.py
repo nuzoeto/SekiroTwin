c936455c516e03d32d38e287071b649a9c150ea1
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-05-20 18:35:36 -0300

    Update afk.py

--- megumin/modulos/afk.py
+++ megumin/modulos/afk.py
@@ -5,8 +5,8 @@ from megumin import megux, Config
 from megumin.utils import get_collection 
 
 
-@megux.on_message(filters.command("afk", Config.TRIGGER))
-async def afk(c: megux, m: Message):
+@megux.on_message(filters.command("afk"))
+async def setwarnlimit_cmd(_, m: Message):
     AFK_STATUS = get_collection(f"_AFK {m.from_user.id}")
     await AFK_STATUS.insert_one({"_id": m.from_user.id}) 
     await m.reply(f"{m.from_user.first_nane} agora está AFK!")
