efa6e422b81c2454c10507e2f32cd7b7df6da457
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-05-20 18:20:40 -0300

    Update afk.py

--- megumin/modulos/afk.py
+++ megumin/modulos/afk.py
@@ -8,8 +8,11 @@ from megumin.utils import get_collection
 @megux.on_message(filters.command("afk", Config.TRIGGER))
 async def afk(c: megux, m: Message):
     AFK_STATUS = get_collection(f"_AFK {m.from_user.id}")
-    afk_msg = f"{m.from_user.first_nane} agora está AFK!"
     query = "on" 
-    await AFK_STATUS.insert_one({"afk": query}) 
-    await m.reply(afk_msg)
+    await AFK_STATUS.insert_one({"_id": m.from_user.id, "afk": query}) 
+    await m.reply(f"{m.from_user.first_nane} agora está AFK!")
+   
+
+
+
     
