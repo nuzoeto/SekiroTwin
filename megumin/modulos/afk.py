8e8559cb53d23f2560daa6f665e128a917c04d0b
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-05-21 15:56:53 -0300

    Update afk.py

--- megumin/modulos/afk.py
+++ megumin/modulos/afk.py
@@ -1,4 +1,8 @@
-from pyrogram import filters 
+import re
+import asyncio 
+
+from pyrogram import filters, enums
+from pyrogram.errors import FloodWait, UserNotParticipant, BadRequest
 from pyrogram.types import Message 
 
 from megumin import megux, Config
@@ -36,10 +40,54 @@ async def rem_afk(c: megux, m: Message):
     
 @megux.on_message(filters.group & ~filters.bot, group=3)
 async def afk_mentioned(c: megux, m: Message):
-    if m.reply_to_message and m.reply_to_message.from_user:
-        AFK_STATUS = get_collection(f"_AFK {m.reply_to_message.from_user.id}")
-        user_afk = await AFK_STATUS.find_one({"_afk": "on"})
-        if not user_afk:
-            return 
-        else:
-            await m.reply(f"{m.reply_to_message.from_user.first_name} está AFK!")
+    if m.entities:
+        for y in m.entities:
+            if y.type == enums.MessageEntityType.MENTION:
+                x = re.search("@(\w+)", m.text)  # Regex to get @username
+                try:
+                    user = await c.get_users(x.group(1))
+                except FloodWait as e:  # Avoid FloodWait
+                    await asyncio.sleep(e.value)
+                except (IndexError, BadRequest, KeyError):
+                    return
+                try:
+                    user_id = user.id
+                    user_first_name = user.first_name
+                except UnboundLocalError:
+                    return
+                except FloodWait as e:  # Avoid FloodWait
+                    await asyncio.sleep(e.value)
+            else:
+                return
+    elif m.reply_to_message and m.reply_to_message.from_user:
+        try:
+            user_id = m.reply_to_message.from_user.id
+            user_first_name = m.reply_to_message.from_user.first_name
+        except AttributeError:
+            return
+    else:
+        return
+
+    try:
+        if user_id == m.from_user.id:
+            return
+    except AttributeError:
+        return
+    except FloodWait as e:  # Avoid FloodWait
+        await asyncio.sleep(e.value)
+
+    try:
+        await m.chat.get_member(user_id)  # Check if the user is in the group
+        pass
+    except UserNotParticipant:
+        return
+
+    AFK = get_collection(f"_AFK {user_id}") 
+    user_afk = await AFK.find_one({"_afk": "on"})
+
+    if not user_afk:
+        return
+    else:
+        afkmsg = (await tld(m, "{} está AFK!")).format(user_first_name)
+        await m.reply_text(afkmsg)
+    await m.stop_propagation()
