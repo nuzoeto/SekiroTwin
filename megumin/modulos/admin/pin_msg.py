7fb1b52d398cba8e792cb988e7a6dbd2437a7a91
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-05-22 16:10:27 -0300

    Update pin_msg.py

--- megumin/modulos/admin/pin_msg.py
+++ megumin/modulos/admin/pin_msg.py
@@ -5,54 +5,53 @@ from megumin import megux
 from megumin.utils import check_bot_rights, check_rights
 
 
-@megux.on_message(filters.command("pin", prefixes=["/", "!"]))
-async def pin_(_, message: Message):
-    chat_id = message.chat.id
-    ids_chat = str(chat_id).replace("-100", "")
-    if not await check_rights(chat_id, message.from_user.id, "can_pin_messages"):
-        await message.reply("Você não tem direitos administrativos suficientes para fazer fixar/desafixar mensagens!")
-        return
-    if not await check_bot_rights(chat_id, "can_pin_messages"):
-        await message.reply("Não consigo fixar mensagens aqui! Verifique se sou administrador e posso fixar mensagens.")
-        return
-    if not message.reply_to_message:
-        await message.reply("Responda a uma mensagem para que eu possa fixa-la")
-        return
-    notifier = disable_notification = True
-    await message.reply_to_message.pin(disable_notification=notifier,)
-    await message.reply(f"""__Eu fixei</b> <a href='t.me/c/{ids_chat}/{message.reply_to_message.message_id}'>esta mensagem</a>.__""", disable_web_page_preview=True,)
+@kuuhaku.on_message(filters.command("pin", trg))
+async def pin_msg(c: kuuhaku, m: Message):
+    input_ = input_str(m).split()
+    reply = m.reply_to_message
+    gid = m.chat.id
+    if not await check_rights(gid, m.from_user.id, "can_pin_messages"):
+        return await m.reply("Você não tem direitos administrativos suficientes para fazer fixar/desafixar mensagens!")
+    if not await check_rights(gid, c.me.id, "can_pin_messages"):
+        return await m.reply("Não consigo fixar mensagens aqui! Verifique se sou administrador e posso fixar mensagens.")
+    if not reply:
+        return await m.reply(await get_string(gid, "PIN_REPLY"))
+    silent = False
+    msg_id = reply.id
+    chat = str(f"{gid}").replace("-100", "")
+    link = f"https://t.me/c/{chat}/{reply.id}"
+    string = await get_string(gid, "PIN")
+    if input_:
+        if ("silent" or "s") in input_:
+            silent = True
+    try:
+        await kuuhaku.pin_chat_message(gid, msg_id, disable_notification=silent)
+        await m.reply(string.format(link))
+    except Exception as e:
+        await kuuhaku.send_err(e)
 
 
-@megux.on_message(filters.command("loudpin", prefixes=["/", "!"]))
-async def pin_(_, message: Message):
-    chat_id = message.chat.id
-    ids_chat = str(chat_id).replace("-100", "")
-    if not await check_rights(chat_id, message.from_user.id, "can_pin_messages"):
-        await message.reply("Você não tem direitos administrativos suficientes para fazer fixar/desafixar mensagens!")
-        return
-    if not await check_bot_rights(chat_id, "can_pin_messages"):
-        await message.reply("Não consigo fixar mensagens aqui! Verifique se sou administrador e posso fixar mensagens.")
-        return
-    if not message.reply_to_message:
-        await message.reply("Responda a uma mensagem para que eu possa fixa-la")
-        return
-    notifier = disable_notification = False
-    await message.reply_to_message.pin(disable_notification=notifier,)
-    await message.reply(f"""__Eu fixei</b> <a href='t.me/c/{ids_chat}/{message.reply_to_message.message_id}'>esta mensagem</a> e notifiquei todos os membros.__""", disable_web_page_preview=True,)
-
-
-@megux.on_message(filters.command("unpin", prefixes=["/", "!"]))
-async def unpin_(_, message: Message):
-    chat_id = message.chat.id
-    ids_chat = str(chat_id).replace("-100", "")
-    if not await check_rights(chat_id, message.from_user.id, "can_pin_messages"):
-        await message.reply("Você não tem direitos administrativos suficientes para fazer fixar/desafixar mensagens!")
-        return
-    if not await check_bot_rights(chat_id, "can_pin_messages"):
-        await message.reply("Não consigo fixar mensagens aqui! Verifique se sou administrador e posso fixar mensagens.")
-        return
-    if not message.reply_to_message:
-        await message.reply("`Responda a uma mensagem para que eu possa desfixa-la`")
-        return
-    await message.reply_to_message.unpin()
-    await message.reply(f"""__Eu desfixei</b> <a href='t.me/c/{ids_chat}/{message.reply_to_message.message_id}'>esta mensagem</a>.__""", disable_web_page_preview=True,)
+@megux.on_message(filters.command("unpin", trg))
+async def pin_msg(c: megux, m: Message):
+    input_ = input_str(m).split()
+    reply = m.reply_to_message
+    gid = m.chat.id
+    if not await check_rights(gid, m.from_user.id, "can_pin_messages"):
+        return await m.reply("Você não tem direitos administrativos suficientes para fazer fixar/desafixar mensagens!")
+    if not await check_rights(gid, c.me.id, "can_pin_messages"):
+        return await m.reply("Não consigo fixar mensagens aqui! Verifique se sou administrador e posso fixar mensagens." )
+    if input_:
+        if "all" in input_:
+            try:
+                return await megux.unpin_all_chat_messages(gid)
+            except Exception as e:
+                await megux.send_log(e)
+        else:
+            pass
+    elif reply:
+        try:
+            return await megux.unpin_chat_message(gid, reply.id)
+        except Exception as e:
+            await megux.send_log(e)
+    else:
+        return await m.reply(await get_string(gid, "UNPIN_REPLY"))
