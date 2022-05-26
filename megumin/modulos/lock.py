0a4eb0af972268082c24ad904398b8975934efd3
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-05-26 18:43:22 -0300

    Update lock.py

--- megumin/modulos/lock.py
+++ megumin/modulos/lock.py
@@ -5,12 +5,14 @@ from megumin import megux, Config
 from megumin.utils import get_collection
 from megumin.utils.decorators import input_str 
 
-LOCK_TYPES = [
-    "audio",
-    "link",
-    "video"
-]
+LOCK_TYPES = ["audio", "link", "video"]
 
 @megux.on_message(filters.command("lock", Config.TRIGGER))
 async def lock(c: megux, m: Message):
-    lock = input_str(m)
+    LOCKS_ = get_collection(f"LOCKED {m.chat.id}")
+    await LOCKS_.insert_one({"lock": "on"})
+
+
+@megux.on_message(filters.command("locktype", Config.TRIGGER))
+async def lock_type(c: megux, m: Message):
+    data = input_str(m)
