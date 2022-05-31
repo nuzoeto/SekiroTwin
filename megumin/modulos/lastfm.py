3ef7fa7adebe125b23c663ed436df601eb8fa5c8
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-05-31 17:01:58 -0300

    Update lastfm.py

--- megumin/modulos/lastfm.py
+++ megumin/modulos/lastfm.py
@@ -81,10 +81,49 @@ async def last_(_, message: Message):
         view_data_ = await get_response.json(link=API, params=params_)
         get_track = view_data_["track"]
         get_scrob = int(get_track["userplaycount"])
-        if get_scrob == 0:
-            scrob = get_scrob + 1
-        else:
-            scrob =  get_scrob          
-            await message.reply(f"{message.from_user.first_name} está ouvindo pela {get_scrob}° Vez.\n<b>{artist_name}<b> - {song_name}")
+        await message.reply(f"{message.from_user.first_name} está ouvindo pela {get_scrob}° Vez.\n<b>{artist_name}<b> - {song_name}")
+          
+
+@megux.on_message(filters.command(["setuser", "reg"], prefixes=["/", "!"]))
+async def last_save_user(_, message: Message):
+    user_id = message.from_user.id
+    fname = message.from_user.first_name
+    uname = message.from_user.username
+    text = input_str(message)
+    if not text:
+        await message.reply("__Bruh.. use /set username.__")
+        return
+    found = await REG.find_one({"id_": user_id})
+    user_start = f"#USER_REGISTER #LOGS\n\n**User:** {fname}\n**ID:** {user_id} <a href='tg://user?id={user_id}'>**Link**</a>"
+    if uname:
+        user_start += f"\n**Username:** @{uname}"
+    if found:
+        await asyncio.gather(
+                REG.update_one({"id_": user_id}, {
+                                "$set": {"last_data": text}}, upsert=True),
+                message.reply("__Your username has been successfully updated.__")
+            )
+    else:
+        await asyncio.gather(
+                REG.update_one({"id_": user_id}, {
+                                "$set": {"last_data": text}}, upsert=True),
+                c.send_log(
+                    user_start,
+                    disable_notification=False,
+                    disable_web_page_preview=True,
+                ),
+                message.reply("__Your username has been successfully set.__")
+            )
 
 
+@megux.on_message(filters.command(["deluser", "duser"], prefixes=["/", "!"]))
+async def last_save_user(_, message: Message):
+    user_id = message.from_user.id
+    found = await REG.find_one({"id_": user_id})
+    if found:
+        await asyncio.gather(
+                REG.delete_one(found),
+                message.reply("__Your username has been deleted.__")
+            )
+    else:
+        return await message.reply("__You don't have a registered username__")
