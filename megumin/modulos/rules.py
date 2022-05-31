43b8c10dcc27b54103a4b76d5f56db9dc0ee9da7
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-05-31 19:57:55 -0300

    Create rules.py

---
+++ megumin/modulos/rules.py
@@ -0,0 +1,17 @@
+from pyrogram import filters
+from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
+
+from megumin import megux
+from megumin.utils import get_collection, get_string 
+
+
+@megux.on_message(filters.command("rules"))
+async def rules_(_, m: Message):
+    data = get_collection(f"RULES  {m.chat.id}")
+    res = await data.find_one()
+    if res:
+        RULES = res["_rules"]
+        await m.reply((await get_string(m.chat.id, "RULES")).format(m.chat.title, RULES))
+    else:
+         await m.reply(await get_string(m.chat.id, "NO_RULES"))
+  
