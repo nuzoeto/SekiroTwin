bd76eb1622683f9b248cd6b492b94788f90b224f
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-06-01 21:44:06 -0300

    Update simi.py

--- megumin/modulos/simi.py
+++ megumin/modulos/simi.py
@@ -12,6 +12,6 @@ async def simi_(_, m: Message):
     text_ = m.text.split(maxsplit=1)[1]
     API = f"https://api.simsimi.net/v2/?text={text_}&lc=pt&cf=false"
     r = requests.get(API).json()  
-    if r["success"] in "Eu não resposta. Por favor me ensine."
+    if r["success"] in "Eu não resposta. Por favor me ensine.":
         return await m.reply("Desconheço esse assunto, mas sei de outros: história, dicas...")
     await m.reply(r["success"])
