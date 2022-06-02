83e7997ae489ea283d9c782a68943136ea65abf3
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-06-02 19:19:18 -0300

    Update disable.py

--- megumin/modulos/disable.py
+++ megumin/modulos/disable.py
@@ -72,17 +72,17 @@ async def enable_cmd(_, m: Message):
         return await m.reply("Esse comando é para ser usado em grupos.")
     else:
         if not query in CMDS:
-            return await m.reply("__Qual comando você deseja ativar?__")
+            return await m.reply(await get_string(m.chat.id, "NO_ENABLE_COMMAND")) 
         else:
             found = await DISABLED.find_one({'_cmd': query})
             if found:
                 if await check_rights(chat_id, check_admin, "can_change_info"):
                     dis_cmd = await DISABLED.delete_one({'_cmd': query})
-                    await m.reply(f"__Comando {query} Agora Ativado!!!__")
+                    await m.reply((await get_string(m.chat.id, "COMMAND_NOW_ENABLED")).format(query))
                 else:
                     return await m.reply("Você não tem direitos administrativos suficientes para alterar dados do grupo!")
             else:
-                return await m.reply("__Comando não desativado!__")
+                return await m.reply(await get_string(m.chat.id, "NO_DISABLED_COMMAND"))
                 
 
 @megux.on_message(filters.command("disableable", Config.TRIGGER))
