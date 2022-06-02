13867b4594694378b004168267be2297ef7ff7cc
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-06-01 22:49:30 -0300

    Update simi.py

--- megumin/modulos/simi.py
+++ megumin/modulos/simi.py
@@ -4,7 +4,7 @@ from pyrogram import filters
 from pyrogram.types import Message 
 
 from megumin import megux, Config
-from megumin.utils import get_collection 
+from megumin.utils import get_collection, get_string 
  
 
 @megux.on_message(filters.command("simi", Config.TRIGGER))
@@ -13,8 +13,8 @@ async def simi_(_, m: Message):
     API = f"https://api.simsimi.net/v2/?text={text_}&lc=pt&cf=false"
     r = requests.get(API).json()  
     if r["success"] in "Eu não resposta. Por favor me ensine.":
-        return await m.reply("Desconheço esse assunto, mas sei de outros: história, dicas...")
+        return await m.reply(await get_string(m.chat.id, "SIMI_NO_RESPONSE"))
     if r["success"]:
         return await m.reply(r["success"])
     else:
-        return await m.reply("A api está apresentado erros inesperados!")
+        return await m.reply(await get_string(m.chat.id, "SIMI_API_OFF"))
