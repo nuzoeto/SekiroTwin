927c3de6e217f059d31633fe3b4b2b4b7657fa34
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-06-03 22:30:39 -0300

    Update telegraph.py

--- megumin/modulos/telegraph.py
+++ megumin/modulos/telegraph.py
@@ -7,13 +7,14 @@ from pyrogram.types import Message
 from telegraph import upload_file
 
 from megumin import megux
+from megumin.utils import get_string
 
 
 @megux.on_message(filters.command(["tg", "telegraph"], prefixes=["/", "!"]))
 async def telegraph_(megux, message: Message):
     replied = message.reply_to_message
     if not replied:
-        await message.reply("`Responda a alguma mídia.`")
+        await message.reply(await get_string(message.chat.id, "TG_NO_REPLY_MEDIA"))
         return
     if not (
         (replied.photo and replied.photo.file_size <= 5242880)
@@ -31,18 +32,18 @@ async def telegraph_(megux, message: Message):
             and replied.document.file_size <= 5242880
         )
     ):
-        await message.reply("`Não suportado!`")
+        await message.reply(await get_string(message.chat.id, "TG_NOT_SUPORTED"))
         return
     download_location = await megux.download_media(
         message=message.reply_to_message, file_name="megumin/xcache/"
     )
-    msg = await message.reply("`Fazendo upload no telegraph...`")
+    msg = await message.reply(await get_string(message.chat.id, "TG_UPLOADING"))
     try:
         response = upload_file(download_location)
     except Exception as document:
         await msg.edit(document)
     else:
-        link = "<b>[Aqui, seu link telegraph!](https://telegra.ph{})</b>"
+        link = await get_string(message.chat.id, "TG_UPLOADED")
         await msg.edit(link.format(response[0]), disable_web_page_preview=True)
     finally:
         os.remove(download_location)
