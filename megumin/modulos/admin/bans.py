5549928ff445485dbc76972ac8a36f9e05487193
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-06-05 16:30:32 -0300

    K

--- megumin/modulos/admin/bans.py
+++ megumin/modulos/admin/bans.py
@@ -80,7 +80,7 @@ async def _ban_user(_, message: Message):
         data = await LOGS.find_one():
         if data:
             id = data["log_id"]
-            await megux.send_message(id, await get_string(chat_id, "BAN_LOGGER")
+            await megux.send_message(id, await get_string(chat_id, "BAN_LOGGER"))
             return
     except Exception as e_f:
         await sent.edit(f"`Algo deu errado 🤔`\n\n**ERROR:** `{e_f}`")
