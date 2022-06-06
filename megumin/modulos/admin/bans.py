4a2bb3c47edadafa98f0453b1b8613445ee81dd7
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-06-05 22:21:17 -0300

    Update bans.py

--- megumin/modulos/admin/bans.py
+++ megumin/modulos/admin/bans.py
@@ -107,9 +107,8 @@ async def _unban_user(_, message: Message):
         await message.reply("`Nenhum User_id válido ou mensagem especificada.`")
         return
     try:
-        user = (await megux.get_users(id_))
-        user_id = user.id
-        mention = user.mention 
+        user_id = (await megux.get_users(id_)).id
+        mention =  (await megux.get_users(id_)).mention
     except (UsernameInvalid, PeerIdInvalid, UserIdInvalid):
         await message.reply(
             "`User_id ou nome de usuário inválido, tente novamente com informações válidas ⚠`"
