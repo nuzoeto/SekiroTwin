71f2ca3c0e92ba11b39c08c3711cef2de7b435f1
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-06-05 21:35:37 -0300

    Update tban.py

--- megumin/modulos/admin/tban.py
+++ megumin/modulos/admin/tban.py
@@ -80,5 +80,10 @@ async def _tban_user(_, message: Message):
         await megux.ban_chat_member(chat_id, user_id, until_date=time_)
         await asyncio.sleep(1)
         await sent.edit("{} foi banido(a) temporariamente por <b>{}</b> em <b>{}</b>\n<b>Motivo</b>: `{}`".format(mention, time_val, message.chat.title, reason or None))
+        data = await LOGS.find_one()
+        if data:
+            id = data["log_id"]
+            await megux.send_message(id, (await get_string(chat_id, "TBAN_LOGGER")).format(message.chat.title, message.from_user.mention(), mention, user_id, time_val,reason or None))
+            return
     except Exception as e_f:  # pylint: disable=broad-except
         await sent.edit(f"`Algo deu errado 🤔`\n\n**ERROR**: `{e_f}`")
