da7bbbe8bbb74a4cbe5b1300f70d79115a52b9fc
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-06-08 22:46:54 -0300

    Update promoter.py

--- megumin/modulos/admin/promoter.py
+++ megumin/modulos/admin/promoter.py
@@ -5,7 +5,7 @@ import asyncio
 
 from pyrogram import filters
 from pyrogram.errors import PeerIdInvalid, UserIdInvalid, UsernameInvalid
-from pyrogram.types import Message
+from pyrogram.types import Message, ChatPrevileges 
 
  
 
@@ -59,11 +59,11 @@ async def _promote_user(_, message: Message):
         await megux.promote_chat_member(
             chat_id,
             user_id,
-            can_change_info=True,
-            can_delete_messages=True,
-            can_restrict_members=True,
-            can_invite_users=True,
-            can_pin_messages=True,
+            ChatPrevileges.can_change_info(True),
+            ChatPrevileges.can_delete_messages(True),
+            ChatPrevileges.can_restrict_members(True),
+            ChatPrevileges.can_invite_users(True),
+            ChatPrevileges.can_pin_messages(True),
         ) 
         if args:
             await asyncio.sleep(2)
