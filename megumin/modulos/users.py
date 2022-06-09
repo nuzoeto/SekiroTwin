164fae3fbc5f38f70ee2ff55a172df01ac347ff8
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-06-09 17:23:03 -0300

    Update users.py

--- megumin/modulos/users.py
+++ megumin/modulos/users.py
@@ -9,6 +9,7 @@ from megumin.utils import get_collection, is_dev
 USERS = get_collection("USERS")
 GROUPS = get_collection("GROUPS")
 USERS_STARTED = get_collection("USERS_START")
+AFK_COUNT = get_collection("AFK_COUNT")
 
 @megux.on_message(filters.command(["status"]))
 async def status_(_, m: Message):
@@ -17,5 +18,6 @@ async def status_(_, m: Message):
         return
     glist = await GROUPS.estimated_document_count()
     ulist = await USERS.estimated_document_count()
+    afk_status = await AFK_COUNT.estimated_document_count()
     userlist = await USERS_STARTED.estimated_document_count()
-    await m.reply(f"**【 WhiterKang Status 】**\n\n**Usuários**: __{userlist}__\n**Regs**: __{ulist}__\n**Grupos**: __{glist}__")
+    await m.reply(f"**【 WhiterKang Status 】**\n\n**Usuários**: __{userlist}__\n**Usuários em AFK**: __{afk_status}__\n**Regs**: __{ulist}__\n**Grupos**: __{glist}__")
