77d1e2991237a819a668964e7531c1628de7951c
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-06-12 17:19:53 -0300

    Update stretch.py

--- megumin/modulos/stretch.py
+++ megumin/modulos/stretch.py
@@ -13,27 +13,11 @@ from megumin.utils.decorators import input_str
 @megux.on_message(filters.command("stretch"))
 async def stretch(c: megux, m: Message):
     text = input_str(m)
-    if not text and m.reply_to_message:
-        if (m.reply_to_message.text or m.reply_to_message.caption) is not None:
-             text = m.reply_to_message.text or m.reply_to_message.caption
-        else:
-             await m.reply_text("`Vou esticar o vento?!`")
-             return
-    
-    if not text and not m.reply_to_message:
-        await m.reply_text("Eu preciso de texto...")
-        return
-    
-        reply = re.sub(
-            r"([aeiouAEIOUａｅｉｏｕＡＥＩＯＵаеиоуюяыэё])",
-            (r"\1" * random.randint(3, 10)),
-            f"{text}",
+
+    reply = re.sub(
+        r"([aeiouAEIOUａｅｉｏｕＡＥＩＯＵаеиоуюяыэё])",
+        (r"\1" * random.randint(3, 10)),
+        f"{text}",
         )
-    
-        try:
-            if m.reply_to_message:
-                await m.reply_to_message.reply_text(reply)
-            else:
-                await m.reply_text(reply)
-        except BadRequest:
-            return await m.reply("Erro")
+    await m.reply_text(reply)
+        
