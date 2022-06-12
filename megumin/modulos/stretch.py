a585f25a179876b62b6cd6c7239995775dab8776
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-06-12 16:59:13 -0300

    Update stretch.py

--- megumin/modulos/stretch.py
+++ megumin/modulos/stretch.py
@@ -12,15 +12,28 @@ from megumin.utils.decorators import input_str
 
 @megux.on_message(filters.command("stretch"))
 async def stretch(c: megux, m: Message):
-    text = input_str(m)
-    if not text or m.reply_to_message.text:
-        return await m.reply("`Vou esticar o vento?!`")
-    if m.reply_to_message.text or m.reply_to_message.caption:
-        text = message.reply_to_message.text or m.reply_to_message.caption 
-    reply = re.sub(
+    text = m.text.split(maxsplit=1)[1]
+    if not text and m.reply_to_message:
+        if (m.reply_to_message.text or m.reply_to_message.caption) is not None:
+             text = m.reply_to_message.text or m.reply_to_message.caption
+        else:
+             await m.reply_text("`Vou esticar o vento?!`")
+             return
+    
+    if not text and not m.reply_to_message:
+        await m.reply_text("Eu preciso de texto...")
+        return
+    
+        reply = re.sub(
             r"([aeiouAEIOUａｅｉｏｕＡＥＩＯＵаеиоуюяыэё])",
             (r"\1" * random.randint(3, 10)),
-            text,
+            f"{text}",
         )
-    await m.reply(reply)
-
+    
+        try:
+            if m.reply_to_message:
+                await m.reply_to_message.reply_text(f"{html.escape(reply)}")
+            else:
+                await m.reply_text(f"{html.escape(reply)}")
+        except BadRequest:
+            return await m.reply("Erro")
