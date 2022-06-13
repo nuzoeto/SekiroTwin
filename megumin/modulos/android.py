2ab8ef3fc57325176512dd48ba59af4cd2089f37
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-06-13 15:41:59 -0300

    Update android.py

--- megumin/modulos/android.py
+++ megumin/modulos/android.py
@@ -160,9 +160,9 @@ async def pixelexperience(c: megux, m: Message):
         build_time = response["datetime"]
 
         text = "<b>Baixar</b>: [{}]({})\n".format(filename, url)
-        text += "<b>Tamanho da compilação</b>: {}\n".format(buildsize_b)
-        text += "<b>Versão</b>: {}\n".format(version)
-        text += "<b>Data</b>: {}\n".format(format_datetime(build_time))
+        text += "<b>Tamanho da compilação</b>: <code>{}</code>\n".format(buildsize_b)
+        text += "<b>Versão</b>: <code>{}</code>\n".format(version)
+        text += "<b>Data</b>: <code>{}</code>\n".format(format_datetime(build_time))
         keyboard = [[InlineKeyboardButton(text="Download", url=url)]]
         await m.reply(text, reply_markup=InlineKeyboardMarkup(keyboard))
     else:
