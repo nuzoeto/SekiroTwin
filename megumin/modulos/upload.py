23c4226f9b7cccc19e7b0de13c60cc18e680a43c
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-06-19 12:41:06 -0300

    Update upload.py

--- megumin/modulos/upload.py
+++ megumin/modulos/upload.py
@@ -89,8 +89,9 @@ async def url_download(message: Message, url: str) -> Tuple[str, int]:
         if count >= 10:
             count = 0
             await msg.edit(f"<b>Downloaded:</b> <i>{percentage}%</i> <b>|</b> <i>{humanbytes(downloaded)}</i>\n<b>ETA:</b> <i>{estimated_total_time}</i>\n<b>Speed:</b> <i>{speed}</i>\n<b>Size:</b> <i>{humanbytes(total_length)}</i>\n<b>Url:</b> <i>{url}</i>", disable_web_page_preview=True)
+            await asyncio.sleep(6.8)
+            await msg.delete()
         await asyncio.sleep(1)
-        await msg.delete()
     return dl_loc, (datetime.now() - start_t).seconds
 
 
