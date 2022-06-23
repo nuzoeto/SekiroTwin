c8b44b53c998affc2fcd47ce295d72c8a715e02f
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-06-23 18:54:10 -0300

    Update warn.py

--- megumin/modulos/warn.py
+++ megumin/modulos/warn.py
@@ -91,6 +91,68 @@ async def warn_cmd(_, m: Message):
         x = input_str(m)
         ids = (await megux.get_users(x)).id
         name_user = (await megux.get_users(x)).mention
+LIMIT = get_collection(f"WARNS_LIMIT {m.chat.id}")
+    ACTION = get_collection(f"ACTION {m.chat.id}")
+    CHAT_LIMIT = await LIMIT.find_one()
+    ACTION_CHAT = await ACTION.find_one()
+    if not CHAT_LIMIT:
+        return await LIMIT.insert_one({"_warnslimit": "3"})
+    if not ACTION_CHAT:
+        return await ACTION.insert_one({"_action": "ban"})
+    GET1 = await LIMIT.find_one({"_warnslimit": "1"})
+    GET2 = await LIMIT.find_one({"_warnslimit": "2"})
+    GET3 = await LIMIT.find_one({"_warnslimit": "3"})
+    GET4 = await LIMIT.find_one({"_warnslimit": "4"})
+    GET5 = await LIMIT.find_one({"_warnslimit": "5"})
+    GET6 = await LIMIT.find_one({"_warnslimit": "6"})
+    GET7 = await LIMIT.find_one({"_warnslimit": "7"})
+    BAN = await ACTION.find_one({"_action": "ban"})
+    KICK = await ACTION.find_one({"_action": "kick"})
+    MUTE = await ACTION.find_one({"_action": "mute"})
+    
+    WARN = get_collection(f"WARN {m.chat.id} {ids}")
+    if await is_self(m.reply_to_message.from_user.id):
+        return await m.reply("Não irei me advertir")
+    if await admin_check(m.reply_to_message):
+        return await m.reply("Não irei advertir um administrador") 
+    if GET1:
+        max_count = 1
+    if GET2:
+        max_count = 2 
+    if GET3: 
+        max_count = 3
+    if GET4:
+        max_count = 4
+    if GET5:
+        max_count = 5
+    if GET6:
+        max_count = 6
+    if GET7:
+        max_count = 7    
+    if not await check_rights(m.chat.id, megux.me.id, "can_restrict_members"):
+        return await m.reply("Eu não tenho permissão suficiente para advertir usuários")
+    if await check_rights(m.chat.id, m.from_user.id, "can_restrict_members"):          
+        name_user = (m.reply_to_message.from_user.mention())
+        await WARN.insert_one({"id_": ids, "title": name_user})
+        WARNS = await WARN.estimated_document_count()
+        if WARNS == max_count: 
+            if BAN:
+                await m.reply(f"{WARNS}/{max_count} Advertencias, {name_user} foi banido!")
+                await WARN.drop()
+                await megux.ban_chat_member(m.chat.id, m.reply_to_message.from_user.id)
+            if KICK:
+                await m.reply(f"{WARNS}/{max_count} Advertencias, {name_user} foi kickado!")
+                await WARN.drop()
+                await megux.ban_chat_member(m.chat.id, m.reply_to_message.from_user.id)
+                await megux.unban_chat_member(m.chat.id, m.reply_to_message.from_user.id)
+            if MUTE:
+                await m.reply(f"{WARNS}/{max_count} Advertencias, {name_user} foi silenciado!")
+                await WARN.drop()
+                await megux.restrict_chat_member(m.chat.id, m.reply_to_message.from_user.id, ChatPermissions())
+        else:
+            await m.reply(f"{name_user} <b>foi advertido!</b>\nEle(a) tem {WARNS}/{max_count} advertências.") 
+    else:
+        return await m.reply("Você não tem permissão suficiente para advertir usuários!")
     elif m.reply_to_message:
         ids = m.reply_to_message.from_user.id 
         name_user = (m.reply_to_message.from_user.mention()) 
