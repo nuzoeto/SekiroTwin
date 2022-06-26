8dcc49721082da40b864e0b3e46b1d50b6df0a6d
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-06-25 23:06:07 -0300

    Update ytdl.py

--- megumin/modulos/ytdl.py
+++ megumin/modulos/ytdl.py
@@ -169,7 +169,7 @@ async def cli_ytdl(c: megux, cq: CallbackQuery):
                 video=filename,
                 width=1920,
                 height=1080,
-                caption=ttemp + yt["title"]},
+                caption=ttemp + yt["title"],
                 duration=yt["duration"],
                 thumb=thumb,
                 reply_to_message_id=int(mid),
