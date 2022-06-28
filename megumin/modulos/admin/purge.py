755c35c997c752177584cf951c7f6d830023ad14
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-06-27 22:45:05 -0300

    Update purge.py

--- megumin/modulos/admin/purge.py
+++ megumin/modulos/admin/purge.py
@@ -4,8 +4,8 @@ from pyrogram.errors import MessageDeleteForbidden
 from pyrogram.types import Message
 from pyrogram.enums import ChatType
 
-from megumin import megux
-from megumin.utils import admin_check
+from megumin import megux, Config
+from megumin.utils import admin_check, check_bot_rights, check_rights, get_collection  
 
 
 @megux.on_message(filters.command("purge"))
@@ -35,3 +35,33 @@ async def purge_command(megux, message: Message):
     else:
         await message.reply("Você precisa ser admin para dar purge.")
         ignore_errors=True
+
+
+@Client.on_message(filters.command("cleanservice", Config.TRIGGER))
+async def delservice(c: Client, m: Message):
+    DATA = get_collection(f"CLEANSERVICE {m.chat.id}")
+    if await check_rights(m.chat.id, "can_delete_messages"):
+        if len(m.text.split()) > 1:
+            if m.command[1] == "on":
+                await DATA.drop()
+                await DATA.insert_one({"status": "on"})
+            elif m.command[1] == "off":
+                await DATA.drop()
+                await DATA.insert_one({"status": "off"})
+            else:
+                return
+        else:
+             return
+
+
+@Client.on_message(filters.service, group=-1)
+async def delservice_action(c: Client, m: Message):
+    DATA = get_collection(f"CLEANSERVICE {m.chat.id")
+    get_delservice = await DATA.find_one({"status": "on"})
+    if not get_delservice:
+        return
+
+    self_member = await m.chat.get_member("me")
+
+    if self_member.privileges and self_member.privileges.can_delete_messages:
+        await m.delete()
