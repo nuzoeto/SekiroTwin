5df5b5abda0f64e833f5207ee51346af722ea668
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-07-09 20:15:45 -0300

    Add files via upload

---
+++ megumin/modulos/wiki.py
@@ -0,0 +1,25 @@
+##BubbalooTeam contribute from WhiterKang
+
+
+##module by DAVI
+
+import wikipedia
+
+from pyrogram import filters
+from pyrogram.types import Message
+
+from megumin import megux, Config
+from megumin.utils.decorators import input_str
+
+@megux.on_command(filters.command("wikipt", Config.TRIGGER))
+async def wikipt(c: megux, m: Message):
+    query = input_str(m)
+    if message.reply_to_message:
+        query = message.reply_to message.text or message.reply_to_message.caption
+    if not query:
+        await m.reply("Ei! Você parado cade os argumentos? Você esqueceu.")
+        return
+    try:
+        wikipedia.set_lang("pt")
+        await m.reply("<b>Resultados da pesquisa no wikipedia:\n\n{}".format(wikipedia.summary))
+        
