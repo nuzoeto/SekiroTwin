2975b3cde7b42c0146ae57c4b181f7587106ad72
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-07-10 20:02:45 -0300

    Update wiki.py

--- megumin/modulos/wiki.py
+++ megumin/modulos/wiki.py
@@ -31,4 +31,23 @@ async def wikipt(c: megux, m: Message):
         return await m.reply("error: {}".format(et))
     except wikipedia.exceptions.DesambiguationError as eet:
         return await m.reply("⚠ Error\n Há muitas coisa! Expresse melhor para achar o resultado!\nPossíveis resultados da consulta:\n{}".format(eet))
+ 
+       
+@megux.on_message(filters.command("wikien", Config.TRIGGER))
+async def wikipt(c: megux, m: Message):
+    if not input_str(m):
+        await m.reply("Ei! Você parado cade os argumentos? Você esqueceu.")
+        return
+    query = m.text 
+    kueri = re.split(pattern="wikien", string=query)
+    try:
+        wikipedia.set_lang("en")
+        keyboard = [[InlineKeyboardButton(text=await tld(m.chat.id, "MOREINFO_BNT"), url=wikipedia.page(kueri).url)]]
+        await m.reply((await tld(m.chat.id, "WIKI_RESULT")).format(wikipedia.summary(kueri, sentences=3)), reply_markup=InlineKeyboardMarkup(keyboard))
+    except wikipedia.PageError as e:
+        return await m.reply("error: {}".format(e))
+    except BadRequest as et:
+        return await m.reply("error: {}".format(et))
+    except wikipedia.exceptions.DesambiguationError as eet:
+        return await m.reply("⚠ Error\n Há muitas coisa! Expresse melhor para achar o resultado!\nPossíveis resultados da consulta:\n{}".format(eet))
         
