338b61861c6c82be7cc97aa35129727b4628b415
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-07-09 21:23:57 -0300

    Update wiki.py

--- megumin/modulos/wiki.py
+++ megumin/modulos/wiki.py
@@ -10,17 +10,17 @@ from pyrogram import filters
 from pyrogram.types import Message
 
 from megumin import megux, Config
-from megumin.utils.decorators import input_str
+
 
 @megux.on_message(filters.command("wikipt", Config.TRIGGER))
 async def wikipt(c: megux, m: Message):
-    query = m.text or ""
+    if len(m.command) == 0:
+        await m.reply("Ei! Você parado cade os argumentos? Você esqueceu.")
+        return
+    query = m.text
     if m.reply_to_message:
         query = m.reply_to_message.text 
     kueri = re.split(pattern="wikipt", string=query)
-    if len(kueri) == 0:
-        await m.reply("Ei! Você parado cade os argumentos? Você esqueceu.")
-        return
     try:
         wikipedia.set_lang("pt")
         await m.reply("<b>Resultados da pesquisa no wikipedia:\n\n{}".format(wikipedia.summary(kueri, sentences=2)))
