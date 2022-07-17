d7cc5e9e28b47becb905dfcf8217ef897b7eeb9d
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-07-17 14:12:59 -0300

    Update lastfm.py

--- megumin/modulos/lastfm.py
+++ megumin/modulos/lastfm.py
@@ -63,3 +63,78 @@ async def last_save_user(_, message: Message):
             )
     else:
         return await message.reply("__You don't have a registered username__")
+
+
+@megux.on_message(filters.command(["profile", "user"]))
+async def now_play(c: megux, message: Message):
+    user_ = message.from_user
+    lastdb = await REG.find_one({"id_": user_.id})
+    if not lastdb:
+        button = InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "Create LastFM account", url="https://www.last.fm/join"
+                    )
+                ]
+            ]
+        )
+        reg_ = "__Enter some username or use /set (username) to set yours. If you don't already have a LastFM account, click the button below to register.__"
+        await message.reply(reg_, reply_markup=button)
+        return
+    user_lastfm = lastdb["last_data"]
+    params = {
+        "method": "user.getinfo",
+        "user": user_lastfm,
+        "api_key": Config.LASTFM_API_KEY,
+        "format": "json",
+    }
+    try:
+        view_data = await get_response.json(link=API, params=params)
+    except ValueError:
+        return await message.reply("__Error. Make sure you entered the correct username__")
+    params_ = {
+        "method": "user.getrecenttracks",
+        "user": user_lastfm,
+        "api_key": Config.LASTFM_API_KEY,
+        "limit": 3,
+        "format": "json",
+    }
+    try:
+        view_scr = await get_response.json(link=API, params=params_)
+    except ValueError:
+        return await message.reply("__Error. Make sure you entered the correct username__")
+
+    # == scrap site
+    url_ = f"https://www.last.fm/user/{user_lastfm}/loved"
+    get_url = requests.get(url_).text
+    soup = bs(get_url, "html.parser")
+    try:
+        scrob = soup.select('h1.content-top-header')[0].text.strip()
+        scrr = scrob.split()[2].replace("(", "").replace(")", "")
+    except IndexError:
+        scrr = None
+    
+    # == user latest scrobbles
+    scr_ = view_scr["recenttracks"]["track"]
+    kek = ""
+    for c in scr_:
+        kek += f"    ♪ **{c['name']}** - __{c['artist']['#text']}__\n"
+
+    # == user data
+    data = view_data["user"]
+    usuario = data["name"]
+    user_url = data["url"]
+    playcount = data["playcount"]
+    country = data["country"]
+    userr = f"<a href='{user_url}'>{usuario}</a>"
+    text_ = f"**{userr} profile**\n"
+    if playcount:
+        text_ += f"**Scrobbles :** {playcount}\n"
+    if country:
+        text_ += f"**Country :** {country}\n"
+    if scrr:
+        text_ += f"**Loved Tracks :** {scrr}\n"
+    if scr_:
+        text_ += f"\n**Latest scrobbles :**\n{kek}"
+    await message.reply(text_, disable_web_page_preview=True)
