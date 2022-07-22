9f6970c90c13694b152e0d1e4303352965bfbf00
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-07-22 16:04:54 -0300

    fix

--- megumin/modulos/android.py
+++ megumin/modulos/android.py
@@ -336,7 +336,7 @@ async def miui_(c: megux, m: Message):
 
     
 @megux.on_message(filters.command("realmeui", Config.TRIGGER))
-async def realmeui(c: Client, message: Message):
+async def realmeui(c: megux, message: Message):
     m = await m.reply_text("__Carregando...__")
     if len(update.command) != 2:
         out_str = "Please write a codename, example: `/realmeui RMX2061`"
