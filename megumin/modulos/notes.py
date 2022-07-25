6217590c82ac59f82308fb8630aacf3505b7bda7
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-07-25 16:50:09 -0300

    Update notes.py

--- megumin/modulos/notes.py
+++ megumin/modulos/notes.py
@@ -13,7 +13,7 @@ from pyrogram import filters
 from pyrogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup, Message, User
 
 from megumin import megux, Config
-from megumin.utils import get_collection, check_rights
+from megumin.utils import get_collection, check_rights, tld
 from megumin.utils.decorators import input_str
 
 BTN_URL_REGEX = re.compile(r"(\[([^\[]+?)\]\(buttonurl:(?:/{0,2})(.+?)(:same)?\))")
@@ -67,13 +67,13 @@ def button_parser(markdown_note):
 
 @megux.on_message(filters.command(["save", "savenote", "note"], Config.TRIGGER))
 async def save_notes(c: megux, m: Message):
+    if not await check_rights(chat_id, user_id, "can_change_info"):
+        return await m.reply(await tld(m.chat.id, "NOTES_NO_PERM"), quote=True)
     if m.reply_to_message is None and len(input_str(m)) < 2:
-        await m.reply_text("Você precisa escrever o nome da nota.", quote=True)
+        await m.reply_text(await tld(m.chat.id, "NOTES_NOT_NAME"), quote=True)
         return
     chat_id = m.chat.id
     user_id = m.from_user.id
-    if not await check_rights(chat_id, user_id, "can_change_info"):
-        return await m.reply("Você não tem permissões suficientes para alterar as notas do grupo.")
     db = get_collection(f"CHAT_NOTES {m.chat.id}")
     args = m.text.html.split(maxsplit=1)
     split_text = f"{args[1]}"
@@ -130,7 +130,7 @@ async def save_notes(c: megux, m: Message):
             raw_data = m.reply_to_message.text
             note_type = "text"
         else:
-            await m.reply("Dê reply em alguma coisa para salvar.")
+            await m.reply(await tld(m.chat.id, "NOTES_NO_REPLY"))
 
     check_note = await db.find_one({"name": trigger})
     if check_note:
@@ -138,7 +138,7 @@ async def save_notes(c: megux, m: Message):
         await db.insert_one({"chat_id": chat_id, "name": trigger, "raw_data": raw_data, "file_id": file_id, "type": note_type})
     else:
         await db.insert_one({"chat_id": chat_id, "name": trigger, "raw_data": raw_data, "file_id": file_id, "type": note_type})
-    await m.reply(f"🗒 <b>A nota</b> <code>{trigger}</code> <b>foi salva com sucesso!</b>")
+    await m.reply(await tld(m.chat.id, "NOTES_SAVED"))
 
 
 @megux.on_message(filters.command("notes", Config.TRIGGER) & filters.group)
