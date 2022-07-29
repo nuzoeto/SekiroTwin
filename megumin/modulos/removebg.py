18ad7bacf7c0f152625417d59a0d843205efbb72
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-07-28 23:17:15 -0300

    Update removebg.py

--- megumin/modulos/removebg.py
+++ megumin/modulos/removebg.py
@@ -30,7 +30,7 @@ async def remove_background(_, message: Message):
         and replied.media
         and (
             replied.photo
-            or (replied.document and "image" in replied.document.mime_type and replied.sticker)
+            or (replied.document and "image" in replied.document.mime_type)
         )
     ):
         msg = await message.reply("Analisando...")
