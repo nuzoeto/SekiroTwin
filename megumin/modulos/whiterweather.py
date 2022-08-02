317f12fad2f78b01ccfd09372adfb985b87ec7d3
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-08-01 23:05:50 -0300

    Update whiterweather.py

--- megumin/modulos/whiterweather.py
+++ megumin/modulos/whiterweather.py
@@ -3,15 +3,12 @@ from pyrogram import filters
 from pyrogram.types import Message
 
 from megumin import megux, Config
-from megumin.utils import get_collection, get_string 
+from megumin.utils import get_collection, get_string, weather_apikey, http
 
-weather_apikey = "8de2d8b3a93542c9a2d8b3a935a2c909"
 
 get_coords = "https://api.weather.com/v3/location/search"
 url = "https://api.weather.com/v3/aggcommon/v3-wx-observations-current"
 
-
-http = httpx.AsyncClient()
 
 headers = {
     "User-Agent": "Dalvik/2.1.0 (Linux; U; Android 13; M2012K11AG Build/SQ1D.211205.017)"
