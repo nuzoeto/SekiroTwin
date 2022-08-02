03f98c888c8993682187d9113b1a7c9348f0f005
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-08-01 22:47:02 -0300

    Update tools.py

--- megumin/utils/tools.py
+++ megumin/utils/tools.py
@@ -147,3 +147,24 @@ async def extract_time(msg, time_val):
     else:
         await msg.reply("`Eu preciso que você informe um tempo (m, h ou d)`")
         return
+
+
+async def cssworker_url(target_url: str):
+    url = "https://htmlcsstoimage.com/demo_run"
+    my_headers = {
+        "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:95.0) Gecko/20100101 Firefox/95.0",
+    }
+
+    data = {
+        "url": target_url,
+        "render_when_ready": False,
+        "viewport_width": 1280,
+        "viewport_height": 720,
+        "device_scale": 1,
+    }
+
+    try:
+        resp = await http.post(url, headers=my_headers, json=data)
+        return resp.json()
+    except HTTPError:
+        return None
