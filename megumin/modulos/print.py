9f853954da72cc980a481c7b3f7f8541aa17fa35
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-08-01 22:46:01 -0300

    Update print.py

--- megumin/modulos/print.py
+++ megumin/modulos/print.py
@@ -5,7 +5,7 @@ from pyrogram import filters
 from pyrogram.types import Message
 
 from megumin import megux
-from megumin.utils import get_collection, get_string  
+from megumin.utils import get_collection, get_string, cssworker_url  
 
 
 http = httpx.AsyncClient()
@@ -118,22 +118,3 @@ async def prints_google(c: megux, message: Message):
         await message.reply("Failed because API is not responding, try again later.")
 
 
-async def cssworker_url(target_url: str):
-    url = "https://htmlcsstoimage.com/demo_run"
-    my_headers = {
-        "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:95.0) Gecko/20100101 Firefox/95.0",
-    }
-
-    data = {
-        "url": target_url,
-        "render_when_ready": False,
-        "viewport_width": 1280,
-        "viewport_height": 720,
-        "device_scale": 1,
-    }
-
-    try:
-        resp = await http.post(url, headers=my_headers, json=data)
-        return resp.json()
-    except HTTPError:
-        return None
