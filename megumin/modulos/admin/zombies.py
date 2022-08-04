0de3c08d146744eb709f7baca13a0a4858c76a9e
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-08-03 23:25:46 -0300

    Update zombies.py

--- megumin/modulos/admin/zombies.py
+++ megumin/modulos/admin/zombies.py
@@ -60,3 +60,43 @@ async def cleanup(c: megux, m: Message):
             await sent.edit_text(await get_string(chat_id, "NO_ZOMBIES"))
     else:
         await m.reply_text(await get_string(chat_id, "NO_RIGHTS_ZOMBIES"))
+
+
+@megux.on_message(filters.command(["cleanup -c", "zombies -c"], prefixes=["/", "!"]))
+async def cleanup(c: megux, m: Message):
+    LOGS = get_collection(f"LOGS {m.chat.id}")
+    chat_id = m.chat.id
+    if m.chat.type == ChatType.PRIVATE:
+        await m.reply_text("Este comando é para ser usado em grupos!")
+        return
+    if not await check_bot_rights(chat_id, "can_restrict_members"):
+        await m.reply(await get_string(chat_id, "NO_BAN_BOT"))
+        return 
+    if await check_rights(chat_id, m.from_user.id, "can_restrict_members"): 
+        count = 0
+        sent = await m.reply_text(await get_string(chat_id, "COM_1"))
+        async for t in c.get_chat_members(chat_id=m.chat.id):
+            if t.user.is_deleted:
+                try:
+                    count += 1
+                except BadRequest:
+                    pass
+                except Forbidden as e:
+                    await m.reply_text(
+                        f"<b>Erro:</b> <code>{e}</code>"
+                    )
+                    return
+        if count:
+            await sent.edit_text(
+                (await get_string(chat_id, "ZOMBIES_BAN")).format(count, m.chat.title)
+            )
+            data = await LOGS.find_one()
+            if data:
+                id = data["log_id"]
+                id_log = int(id)
+                await megux.send_message(id_log, (await get_string(chat_id, "ZOMBIES_LOGGER")).format(m.chat.title, m.from_user.mention(), count))
+                return
+        else:
+            await sent.edit_text(await get_string(chat_id, "NO_ZOMBIES"))
+    else:
+        await m.reply_text(await get_string(chat_id, "NO_RIGHTS_ZOMBIES"))
