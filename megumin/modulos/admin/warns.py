3e09e29a08c51f3170c3c302f260dc7d80d27f0c
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-08-10 22:20:09 -0300

    Update warns.py

--- megumin/modulos/admin/warns.py
+++ megumin/modulos/admin/warns.py
@@ -99,3 +99,54 @@ async def warn_users(_, message: Message):
         await DB_WARNS.delete_many({"user_id": user_id})
     else:
         await message.reply(f"{mention} <b>foi advertido!</b>\nEle(a) têm {user_warns}/{warns_limit} Advertências.\n<b>Motivo:</b> {reason or None}")
+        
+        
+@megux.on_message(filters.command("unwarn", Config.TRIGGER))
+async def unwarn_users(_, message: Message):
+    chat_id = message.chat.id
+    if not await check_rights(chat_id, message.from_user.id, "can_restrict_members"):
+        await message.reply(await get_string(chat_id, "NO_BAN_USER"))
+        return
+    cmd = len(message.text)
+    replied = message.reply_to_message
+    reason = ""
+    if replied:
+        id_ = replied.from_user.id
+        if cmd > 7:
+            _, reason = message.text.split(maxsplit=1)
+    elif cmd > 7:
+        _, args = message.text.split(maxsplit=1)
+        if " " in args:
+            id_, reason = args.split(" ", maxsplit=1)
+        else:
+            id_ = args
+    else:
+        await message.reply(await get_string(message.chat.id, "BANS_NOT_ESPECIFIED_USER"))
+        return
+    try:
+        user = await megux.get_users(id_)
+        user_id = user.id
+        mention = user.mention
+    except (UsernameInvalid, PeerIdInvalid, UserIdInvalid):
+        await message.reply(
+            await get_string(message.chat.id, "BANS_ID_INVALID")
+        )
+        return
+    if await is_self(user_id):
+        await message.reply(await get_string(chat_id, "BAN_MY_SELF"))
+        await sed_sticker(message)
+        return 
+    if is_dev(user_id):
+        await message.reply(await get_string(chat_id, "BAN_IN_DEV"))
+        return
+    if await is_admin(chat_id, user_id):
+        await message.reply(await get_string(chat_id, "BAN_IN_ADMIN"))
+        return
+    if not await check_rights(chat_id, megux.me.id, "can_restrict_members"):
+        await message.reply(await get_string(chat_id, "NO_BAN_BOT"))
+        await sed_sticker(message)
+        return
+    DB_WARNS = get_collection(f"WARNS {message.chat.id}")
+    #delete one warn--user
+    await DB_WARNS.delete_one({"user_id": user_id})
+    await message.reply("A última advertencia foi removida!")
