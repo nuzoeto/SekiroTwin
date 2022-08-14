8440c6e3cb73d5fbb0d9395e18cd49b21270a7dd
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-08-14 16:40:24 -0300

    Update help.py

--- megumin/modulos/help.py
+++ megumin/modulos/help.py
@@ -37,32 +37,32 @@ async def help(client, message):
     button = InlineKeyboardMarkup(
         [
             [
-                InlineKeyboardButton("Admin", callback_data="help_admin"),
-                InlineKeyboardButton("Anilist", callback_data="help_ani"),
-                InlineKeyboardButton("Android", callback_data="help_andr"),
+                InlineKeyboardButton(await get_string(cb.message.chat.id, "BNT_1"), callback_data="help_admin"),
+                InlineKeyboardButton(await get_string(cb.message.chat.id, "BNT_2"), callback_data="help_ani"),
+                InlineKeyboardButton(await get_string(cb.message.chat.id, "BNT_3"), callback_data="help_andr"),
             ],
             [
-                InlineKeyboardButton("Outros", callback_data="help_misc"),
-                InlineKeyboardButton("Geral", callback_data="help_geral"),
-                InlineKeyboardButton("Lastfm", callback_data="help_last"),
+                InlineKeyboardButton(await get_string(cb.message.chat.id, "BNT_4"), callback_data="help_misc"),
+                InlineKeyboardButton(await get_string(cb.message.chat.id, "BNT_5"), callback_data="help_geral"),
+                InlineKeyboardButton(await get_string(cb.message.chat.id, "BNT_6"), callback_data="help_last"),
             ],
             [
-                InlineKeyboardButton("Notas", callback_data="help_notes"),
-                InlineKeyboardButton("Purges", callback_data="help_purges"),
-                InlineKeyboardButton("Youtube", callback_data="help_yt"),
+                InlineKeyboardButton(await get_string(cb.message.chat.id, "BNT_7"), callback_data="help_notes"),
+                InlineKeyboardButton(await get_string(cb.message.chat.id, "BNT_8"), callback_data="help_purges"),
+                InlineKeyboardButton(await get_string(cb.message.chat.id, "BNT_9"), callback_data="help_yt"),
             ],
             [
-                InlineKeyboardButton("Bans", callback_data="help_bans"),
-                InlineKeyboardButton("GitHub", callback_data="help_git"),
-                InlineKeyboardButton("Memes", callback_data="help_memes"),
+                InlineKeyboardButton(await get_string(cb.message.chat.id, "BNT_10"), callback_data="help_bans"),
+                InlineKeyboardButton(await get_string(cb.message.chat.id, "BNT_11"), callback_data="help_git"),
+                InlineKeyboardButton(await get_string(cb.message.chat.id, "BNT_12"), callback_data="help_memes"),
             ],
             [
-                InlineKeyboardButton("Tradutor", callback_data="help_tr"),
-                InlineKeyboardButton("Stickers", callback_data="help_stickers"),
-                InlineKeyboardButton("Desativar", callback_data="help_disable"),
+                InlineKeyboardButton(await get_string(cb.message.chat.id, "BNT_13"), callback_data="help_tr"),
+                InlineKeyboardButton(await get_string(cb.message.chat.id, "BNT_14"), callback_data="help_stickers"),
+                InlineKeyboardButton(await get_string(cb.message.chat.id, "BNT_15"), callback_data="help_disable"),
             ],
             [
-                InlineKeyboardButton("Boas Vindas", callback_data="help_welcome"),
+                InlineKeyboardButton(await get_string(cb.message.chat.id, "BNT_16"), callback_data="help_welcome"),
             ],
         ]
     )
