366fb439148bef6468421a19f2153744fb4834bd
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-08-17 22:49:00 -0300

    Update welcome.py

--- megumin/modulos/admin/welcome.py
+++ megumin/modulos/admin/welcome.py
@@ -15,7 +15,7 @@ from typing import Callable, List, Optional, Union
 
 
 from megumin import megux, Config
-from megumin.utils import get_collection, check_rights
+from megumin.utils import get_collection, check_rights, check_bot_rights, is_admin
 
 
 
