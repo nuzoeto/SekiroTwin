dc39ef8298f686ba72962417f90e31e911ee3c72
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-08-19 14:57:58 -0300

    Update disable.py

--- megumin/modulos/disable.py
+++ megumin/modulos/disable.py
@@ -87,8 +87,38 @@ async def enable_cmd(_, m: Message):
 
 @megux.on_message(filters.command("disableable", Config.TRIGGER))
 async def disableable(_, m: Message):
-    text = "<b>Comandos Disponiveis para ser desativados</b>:"
-    for command in sorted(DISABLABLE_CMDS):
-        text += f"* <code>/{command}</code>\n"
+    text = """<b>Comandos Disponiveis para ser desativados</b>
+- __ban__
+- __banme__
+- __ddd__
+- __dicio__
+- __cep__
+- __clima__
+- __getsticker__
+- __ip__
+- __insults__
+- __kang__
+- __kick__
+- __kickme__ 
+- __mute__
+- __muteme__
+- __ping__
+- __pypi__
+- __print__
+- __tr__
+- __report__
+- __reverse__
+- __slap__
+- __short__
+- __simi__
+- __stickerid__
+- __tmute__
+- __song(já desativado)__
+- __unban__
+- __unmute__
+- __vapor__
+- __video(já desativado)__
+"""
+    
     await m.reply(text)
 
