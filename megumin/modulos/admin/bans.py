557ca914570fa3d5c9aa98f15904599216130d98
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-08-19 12:07:33 -0300

    Update bans.py

--- megumin/modulos/admin/bans.py
+++ megumin/modulos/admin/bans.py
@@ -19,18 +19,15 @@ from megumin.utils import (
     sed_sticker,
     get_collection,
     get_string,
+    is_disabled,
 )
 
 
 @megux.on_message(filters.command("ban", prefixes=["/", "!"]))
 async def _ban_user(_, message: Message):
-    DISABLED = get_collection(f"DISABLED {message.chat.id}")
     LOGS = get_collection(f"LOGS {message.chat.id}")
-    query = "ban"
-    off = await DISABLED.find_one({"_cmd": query})
-    if off:
-        return
     chat_id = message.chat.id
+    await is_disabled(chat_id, "ban")
     if not await check_rights(chat_id, message.from_user.id, "can_restrict_members"):
         await message.reply(await get_string(chat_id, "NO_BAN_USER"))
         return
