cda8b5e2dd989c67170cd4142c9ef421915a9cac
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-08-19 12:25:59 -0300

    Update disabled.py

--- megumin/utils/database/disabled.py
+++ megumin/utils/database/disabled.py
@@ -1,7 +1,7 @@
 from megumin.utils import get_collection
 
 
-async def is_disabled(gid: int, query: str):
+async def is_disabled(gid: int, query: str) -> bool:
     DISABLED = get_collection(f"DISABLED {gid}")
     off = await DISABLED.find_one({"_cmd": query})
-    
+    return bool(off)
