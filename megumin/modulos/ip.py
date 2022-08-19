c5d914bd6682a0e5f8f5aeeca7602b9a92a22ae9
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-08-19 14:37:04 -0300

    Update ip.py

--- megumin/modulos/ip.py
+++ megumin/modulos/ip.py
@@ -14,7 +14,6 @@ http = httpx.AsyncClient()
 
 
 @megux.on_message(filters.command("ip", Config.TRIGGER))
-@disableable_dec("ip")
 async def ip_cmd_(c: megux, m: Message):
     query = "ip"
     if await is_disabled(m.chat.id, query):
