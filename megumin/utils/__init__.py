1e43d0e6680ca6707f0903c55592c500c2c1b43c
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-08-20 12:22:40 -0300

    Update __init__.py

--- megumin/utils/__init__.py
+++ megumin/utils/__init__.py
@@ -2,6 +2,7 @@ from .database import get_collection
 from .database.lang import get_string, add_lang # nao_meche
 from .database.lang import get_string as tld # nao_meche
 from .database.disabled import is_disabled # nao_meche
+from .database.ytdl import csdl, cisdl, tsdl, tisdl #nao_meche
 from .functions import rand_array  # nao_meche
 from .logger import logging
 from .tools import extract_time  # nao_meche
@@ -23,4 +24,4 @@ from .tools import (
     escape_definition,
     unwarn_bnt,
 )
-from .aiohttp import AioHttp as get_response
+from .aiohttp import AioHttp as get_response #nao_meche
