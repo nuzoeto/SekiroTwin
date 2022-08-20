ac057ca583f724c808067ce73fbb8c4061a33c4f
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-08-20 16:43:10 -0300

    Update report.py

--- megumin/modulos/report.py
+++ megumin/modulos/report.py
@@ -31,8 +31,11 @@ async def report_admins(c: megux, m: Message):
             admins_ = ""
             async for i in m.chat.get_members(filter=ChatMembersFilter.ADMINISTRATORS):
                 if not (i.user.is_deleted, i.privileges.is_anonymous, i.user.is_bot):
-                    await c.send_message(i.user.id, f"{user} está chamando os administradores no {chat}")
                     mention += f"<a href='tg://user?id={i.user.id}'>\u2063</a>"
+                    id = i.user.id
+                    while True:
+                        await megux.send_message(id, f"{user} está chamando os administradores em {chat}")
+                        pass
             await m.reply_to_message.reply_text(
                 "{admins_list}{reported_user} reportado para os administradores.".format(
                     admins_list=mention,
