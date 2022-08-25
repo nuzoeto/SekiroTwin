a637bd88d374a09c7597f3807eeb6a6bf466f779
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-08-24 23:34:19 -0300

    Create afk.py

---
+++ megumin/utils/database/afk.py
@@ -0,0 +1,27 @@
+from megumin.utils import get_collection, tld
+
+
+async def check_afk(m, user_id, user_fn, user):
+    if user_id == user.id:
+        return
+      
+    AFK = get_collection(f"_AFK {user_id}")
+    REASON = get_collection(f"REASON {user_id}")
+      
+    if await AFK.find_one({"_afk": "on"}):
+        try:
+            await m.chat.get_member(int(user_id))  # Check if the user is in the group
+        except (UserNotParticipant, PeerIdInvalid):
+            return
+
+        
+        if await REASON.find_one():
+            db = await REASON.find_one()
+            r = db["reason"]
+            afkmsg = (await tld(m.chat.id, "AFK_IS_NOW_REASON")).format(user_fn, r)
+        else:
+            afkmsg = (await tld(m.chat.id, "AFK_IS_NOW")).format(user_fn)
+        try:
+            return await m.reply_text(afkmsg)
+        except ChatWriteForbidden:
+            return
