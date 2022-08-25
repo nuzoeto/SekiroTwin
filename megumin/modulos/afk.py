299b431ebb95aec9947cc67dc47b1b7717c49c93
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-08-24 22:54:25 -0300

    Update afk.py

--- megumin/modulos/afk.py
+++ megumin/modulos/afk.py
@@ -69,7 +69,7 @@ async def rem_afk(c: megux, m: Message):
     )
 
     
-@megux.on_message(filters.group & ~filters.bot, group=2)
+@megux.on_message(filters.group & ~filters.bot, group=6)
 async def afk_mentioned(c: megux, m: Message):
     if m.entities:
         for y in m.entities:
