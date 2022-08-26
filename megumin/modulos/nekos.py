f97668bac2c9309db5c50ff90e912f676fb703fb
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-08-26 17:17:46 -0300

    Update nekos.py

--- megumin/modulos/nekos.py
+++ megumin/modulos/nekos.py
@@ -58,7 +58,7 @@ async def baka_(_, message: Message):
     if qu:
         msg = await message.reply(f"<i>Searching wallpapers...</i> <b>{qu}</b>")
         try:
-            results = requests.get(f"https://kuuhaku-api.ddns/api/wallpaper?query={qu}")
+            results = requests.get(f"https://kuuhaku-api.ddns.net/api/mobilewall?query={qu}")
             _json = results.json()['url']
         except Exception:
             return await msg.edit("<i>Não foi possivel fazer a busca, tente novamente.</i>")
