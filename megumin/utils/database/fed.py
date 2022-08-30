e978dda1b6f98d4b924f94d464f085e78e6088d2
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-08-30 19:37:38 -0300

    Update fed.py

--- megumin/utils/database/fed.py
+++ megumin/utils/database/fed.py
@@ -1,6 +1,18 @@
 from megumin.utils import get_collection
+from pyrogram.types import Message
 
 feds = get_collection("FEDERATION")
 
-async def new_fed_db(new_fed, fed_id, created_time, owner_id):
+async def new_fed(m: Message, fed_name, fed_id, owner_id):
     GETFED = await feds.find_one({"owner_id": owner_id})
+    if GETFED:
+        await m.reply("Ei você já tem uma federação. Não é possivel criar outra renomeie a federação ou exclua a atual para criar outra.")
+        return
+    await feds.insert_one({"fed_name": fed_name, "fed_id": fed_id, "owner_id": owner_id})
+    await m.reply(
+        "*You have successfully created a new federation!*"
+        "\nName: `{}`"
+        "\nID: `{}`"
+        "\n\nUse the command below to join the federation:"
+        "\n`/joinfed {}`".format(fed_name, fed_id, fed_id)
+        )
