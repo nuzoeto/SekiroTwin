3ed04c362a5fd37001bf9d3645c041291c6ff824
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-08-30 21:00:53 -0300

    Update feds.py

--- megumin/modulos/feds.py
+++ megumin/modulos/feds.py
@@ -14,14 +14,14 @@ from pyrogram.enums import ChatType
 
 
 @megux.on_message(filters.command(["newfed", "fednew", "fnew"], Config.TRIGGER))
-async def create_new_fed(m: Message, c: megux):
+async def create_new_fed(c: megux, m: Message):
     if m.chat.type == (ChatType.SUPERGROUP, ChatType.GROUP):
         return await m.reply("You can your federation in my PM, not in a group.")
-    if not (len(message.command) >= 2):
-        return await message.reply("Give your federation a name!")
-    if (len(' '.join(message.command[1:])) > 60):
-        return await message.reply("Your fed must be smaller than 60 words.")
-    fed_name = ' '.join(message.command[1:])
+    if not (len(m.command) >= 2):
+        return await m.reply("Give your federation a name!")
+    if (len(' '.join(m.command[1:])) > 60):
+        return await m.reply("Your fed must be smaller than 60 words.")
+    fed_name = ' '.join(m.command[1:])
     fed_id = str(uuid.uuid4())
-    owner_id = message.from_user.id 
+    owner_id = m.from_user.id 
     await new_fed(m, fed_name, fed_id, owner_id)
