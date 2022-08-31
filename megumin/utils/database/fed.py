c715a682fa7534bb24aff682d64f2d89a8630d8c
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-08-30 21:40:58 -0300

    added funcs

--- megumin/utils/database/fed.py
+++ megumin/utils/database/fed.py
@@ -3,6 +3,7 @@ from megumin.utils import get_collection
 from pyrogram.types import Message
 
 feds = get_collection("FEDERATION")
+federation = get_collection("FEDERATION_GROUP")
 
 async def new_fed(m: Message, fed_name, fed_id, owner_id):
     GETFED = await feds.find_one({"owner_id": owner_id})
@@ -23,4 +24,60 @@ async def new_fed(m: Message, fed_name, fed_id, owner_id):
     await megux.send_log("Federation <b>{}</b> has been created with ID: <pre>{}</pre>".format(
                     fed_name, fed_id
         ),
+    )
+
+    
+async def join_fed(chat_id, chat_title, fed_id):
+    await federation.update_one(
+        {
+            'fed_id': fed_id
+        },
+        {
+            "$addToSet": {
+                'chats': {
+                    "$each": [
+                        {
+                            'chat_id': chat_id,
+                            'chat_title': chat_title
+                        }
+                    ]
+                }
+            }
+        }
+    )
+    
+async def leave_fed(chat_id, chat_title, fed_id):
+    await federation.update(
+        {
+            'fed_id': fed_id
+        },
+        {
+             "$remove": {
+                'chats': {
+                    "$pull": [
+                        {
+                            'chat_id': chat_id  ,
+                            'chat_title': chat_title
+                        }
+                    ]
+                }
+            }
+        }
+    )
+   
+async def user_fban(fed_id, user_id, reason):
+    await feds.update(
+        {
+            'fed_id': fed_id
+        },
+        {
+            "$set": {
+                'banned_users': [
+                    {
+                        'user_id': user_id,
+                        'reason': reason
+                    }
+                ]
+            }
+        }
     )
