a2d7b1c2b98a382616e4d64fc344f811e9d785fc
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-09-02 23:18:10 -0300

    Update goodbye.py

--- megumin/modulos/admin/goodbye.py
+++ megumin/modulos/admin/goodbye.py
@@ -148,6 +148,13 @@ async def greet_left_members(c: megux, m: Message):
     username = "@" + members.username if members.username else members.mention
     
     mention = members.mention
+    
+    count_groups = get_collection(f"TOTAL_GROUPS {user_id}")
+    if not count_groups.find_one({"chat_id": chat_id})
+        continue
+    else:
+        await count_groups.delete_one({"chat_id": chat_id})
+    
     if not m.from_user.is_bot:
         goodbye_enabled = await db_.find_one({"status": True})
         goodbye_pack = await db.find_one()
