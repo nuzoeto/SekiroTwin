7f60c824a1c39c7c0373562945b380b3b96c891a
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-09-03 08:43:57 -0300

    add user to count in whois

---
+++ megumin/utils/database/info.py
@@ -0,0 +1,6 @@
+from megumin.utils import get_collection
+
+async def add_user_count(chat_id, user_id):
+    count_groups = get_collection(f"TOTAL_GROUPS {user_id}")
+    if not await count_groups.find_one({"chat_id": chat_id}):
+        await count_groups.insert_one({"chat_id": chat_id})
