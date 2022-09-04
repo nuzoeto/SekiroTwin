43d9274751c760fffa17d1ad3d150c7a84a31441
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-09-04 14:52:34 -0300

    Update info.py

--- megumin/utils/database/info.py
+++ megumin/utils/database/info.py
@@ -1,6 +1,24 @@
 from megumin.utils import get_collection
 
 async def add_user_count(chat_id, user_id):
-    count_groups = get_collection(f"TOTAL_GROUPS {user_id}")
-    if await count_groups.find_one({"chat_id": chat_id}):
-        await count_groups.drop()
+    add_groups = get_collection(f"TOTAL_GROUPS")
+    try:
+        if not await add_groups.find_one({"user_id": user_id, "chat_id": chat_id}):
+            await add_groups.insert_one({"user_id": user_id, "chat_id": chat_id})
+    except Exception
+        pass
+
+async count_groups_user(chat_id, user_id):
+    count_groups = get_collection(f"TOTAL GROUPS")
+    num = 0
+    async for count in count_groups.find():
+        num += 1
+    return num
+
+async def add_user_count(chat_id, user_id):
+    add_groups = get_collection(f"TOTAL_GROUPS")
+    try:
+        if await add_groups.find_one({"user_id": user_id, "chat_id": chat_id}):
+            await add_groups.delete_one({"user_id": user_id, "chat_id": chat_id})
+    except Exception
+        pass
