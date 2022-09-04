f53407913bcb19e4cfd3e3200ee3a4a57576089c
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-09-04 12:29:34 -0300

    Update info.py

--- megumin/utils/database/info.py
+++ megumin/utils/database/info.py
@@ -3,4 +3,4 @@ from megumin.utils import get_collection
 async def add_user_count(chat_id, user_id):
     count_groups = get_collection(f"TOTAL_GROUPS {user_id}")
     if not await count_groups.find_one({"chat_id": chat_id}):
-        await count_groups.insert_one({"chat_id": chat_id})
+        await count_groups.find_one({"chat_id": chat_id})
