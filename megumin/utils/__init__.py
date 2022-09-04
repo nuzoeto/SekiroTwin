8159e7c3ee36d99c27558a021099795c05db51a4
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-09-04 15:03:38 -0300

    Update __init__.py

--- megumin/utils/__init__.py
+++ megumin/utils/__init__.py
@@ -4,7 +4,7 @@ from .database.lang import get_string as tld # nao_meche
 from .database.disabled import is_disabled # nao_meche
 from .database.ytdl import csdl, cisdl, tsdl, tisdl #nao_meche
 from .database.afk import check_afk
-from .database.info import add_user_count
+from .database.info import add_user_count, del_user_count, count_groups_user #nao_meche
 from .database.fed import new_fed, join_fed, leave_fed, user_fban, is_user_fban, update_reason, get_fed_from_chat, get_fed_from_ownerid, user_unfban
 from .functions import rand_array  # nao_meche
 from .logger import logging
