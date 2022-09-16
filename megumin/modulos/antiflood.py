69ede7ed38aea732c6023fd3556ce087140e4356
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-09-16 10:28:04 -0300

    Update antiflood.py

--- megumin/modulos/antiflood.py
+++ megumin/modulos/antiflood.py
@@ -28,7 +28,7 @@ async def flood_control_func(_, message: Message):
     if not await DB.find_one({"chat_id": chat_id, "status": "on"}):
         return
     if limit:
-        chat_limit = limit["limit"]
+        chat_limit = int(limit["limit"])
     else:
         chat_limit = 5
     if chat_id not in MSGS_CACHE:
@@ -41,7 +41,7 @@ async def flood_control_func(_, message: Message):
     if user_id not in MSGS_CACHE[chat_id]:
         MSGS_CACHE[chat_id][user_id] = 0
     reset_flood(chat_id, user_id)
-    if MSGS_CACHE[chat_id][user_id] >= :
+    if MSGS_CACHE[chat_id][user_id] >= chat_limit:
         MSGS_CACHE[chat_id][user_id] = 0
         try:
             if is_admin(chat_id, user_id):
