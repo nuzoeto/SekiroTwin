11e33ca217ff82d755beb414aaeaf193259871b4
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-09-16 11:27:45 -0300

    Update __main__.py

--- megumin/__main__.py
+++ megumin/__main__.py
@@ -1,10 +1,23 @@
 # Copyright (C) 2022 by fnixdev
 #
-
+import logging
 from .bot import megux
 from pyrogram import idle
 import asyncio
 from .utils.database.lang import load_language
+
+logging.basicConfig(level=logging.INFO,
+                    format='[%(asctime)s - %(levelname)s] - %(name)s - %(message)s',
+                    datefmt='%d-%b-%y %H:%M:%S',
+                    handlers=[
+                        RotatingFileHandler(
+                            "WhiterKang.log", maxBytes=20480, backupCount=10),
+                        logging.StreamHandler()
+                    ])
+
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+logging.getLogger("pyrogram.parser.html").setLevel(logging.ERROR)
+logging.getLogger("pyrogram.session.session").setLevel(logging.ERROR)
 
 
 async def main():
@@ -15,4 +28,9 @@ async def main():
     
 
 if __name__ == "__main__" :
-    asyncio.get_event_loop().run_until_complete(main())
+    try:
+        asyncio.get_event_loop().run_until_complete(main())
+    except Exception as err:
+        logging.error(err.with_traceback(None))
+    finally:
+        asyncio.get_event_loop().stop()
