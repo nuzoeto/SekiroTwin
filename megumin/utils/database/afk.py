abdd8b93642cb7d1fb662f43c7aa31a4358c54c2
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-09-16 23:07:51 -0300

    Update afk.py

--- megumin/utils/database/afk.py
+++ megumin/utils/database/afk.py
@@ -5,17 +5,17 @@ async def check_afk(m, user_id, user_fn, user):
     if user_id == user.id:
         return
       
-    AFK = get_collection(f"_AFK {user_id}")
-    REASON = get_collection(f"REASON {user_id}")
+    AFK = get_collection("_AFK")
+    REASON = get_collection("REASON_AFK")
       
-    if await AFK.find_one({"_afk": "on"}):
+    if await AFK.find_one({"user_id": user_id, "_afk": "on"}):
         try:
             await m.chat.get_member(int(user_id))  # Check if the user is in the group
         except (UserNotParticipant, PeerIdInvalid):
             return
 
         
-        if await REASON.find_one():
+        if await REASON.find_one({"user_id": user_id}):
             db = await REASON.find_one()
             r = db["_reason"]
             afkmsg = (await tld(m.chat.id, "IS_AFK_REASON")).format(user_fn, r)
