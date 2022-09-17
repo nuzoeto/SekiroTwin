ee5c731919c5126d098aadc7b15ae5dd576b82ed
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-09-17 10:28:46 -0300

    Update rules.py

--- megumin/modulos/rules.py
+++ megumin/modulos/rules.py
@@ -7,8 +7,8 @@ from megumin.utils import get_collection, get_string
 
 @megux.on_message(filters.command("rules", Config.TRIGGER))
 async def rules_(_, m: Message):
-    data = get_collection(f"RULES {m.chat.id}")
-    res = await data.find_one()
+    data = get_collection(f"RULES")
+    res = await data.find_one({"chat_id": m.chat.id})
     if res:
         RULES = res["_rules"]
         await m.reply((await get_string(m.chat.id, "RULES")).format(m.chat.title, RULES), disable_web_page_preview=True)
