1cba874a124d4b00400f5c43199457ae58831a89
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-09-18 09:59:28 -0300

    Update functions.py

--- megumin/utils/functions.py
+++ megumin/utils/functions.py
@@ -1,4 +1,5 @@
 import random
+from re import findall
 
 # funções futuras
 
@@ -6,3 +7,11 @@ import random
 def rand_array(array: list, string: bool = True):
     random_num = random.choice(array)
     return str(random_num) if string else random_num
+
+
+def get_urls_from_text(text: str) -> bool:
+    regex = r"""(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]
+                [.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(
+                \([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\
+                ()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))""".strip()
+    return [x[0] for x in findall(regex, str(text))]
