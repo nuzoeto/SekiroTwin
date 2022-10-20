e1723fcdb97989766eb512043841d7c7e3c99d78
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-10-20 19:43:47 -0300

    Update deviceinfo.py

--- megumin/modulos/deviceinfo.py
+++ megumin/modulos/deviceinfo.py
@@ -13,12 +13,12 @@ async def deviceinfo(c: megux, m: Message):
     if input_str(m):
         name = m.text.split(" ", 1) 
         search = f"{name}".replace(" ", "%")
-        get_search_api = await http.get(f"http://api.davitudo.tk/search/{search}").json()
+        get_search_api = (await http.get(f"http://api.davitudo.tk/search/{search}")).json()
         if get_search_api == "[]":
             return await m.reply("<code>Não encontrei esse dispositivo!!</code> <i>:(</i>")
         id = get_search_api["url"]
         img = get_search_api["img"]
-        get_device_api = await http.get(f"http://api.davitdo.tk/device/{id}")
+        get_device_api = (await http.get(f"http://api.davitdo.tk/device/{id}"))
         try:
             name_cll = get_device_api["title"]
             await m.reply(f"<b>Foto Device</b>: {img}\n<b>URL Fonte:</b>: https://www.gsmarena.com/{id}", disable_web_page_preview=False)
