da01777679f7d9907bcb39d009f3eddb47fad892
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-10-21 12:28:47 -0300

    Update deviceinfo.py

--- megumin/modulos/deviceinfo.py
+++ megumin/modulos/deviceinfo.py
@@ -24,7 +24,7 @@ async def deviceinfo(c: megux, m: Message):
             name_cll = get_device_api['title']
             network = get_device_api['spec_detail'][0]['specs'][0]['value']
             anonciament = get_device_api['spec_detail'][1]['specs'][0]['value']
-            await m.reply(f"<b>Foto Device</b>: {img}\n<b>URL Fonte:</b>: https://www.gsmarena.com/{id}\n\n<b>- Aparelho</b>:  <i>{name_cll}</i>\n<b>- Anunciado</b>: <i>{anonciament}</i>", disable_web_page_preview=False)
+            await m.reply(f"<b>Foto Device</b>: {img}\n<b>URL Fonte:</b>: https://www.gsmarena.com/{id}\n\n<b>- Aparelho</b>:  <i>{name_cll}</i>\n<b>- Lançamento</b>: <i>{anonciament}</i>\n<b>- Redes</b>: <i>{network}</i>", disable_web_page_preview=False)
         except Exception as err:
             return await m.reply("Não consegui obter resultados sobre o aparelho. O gsmarena pode estar offline. <i>Erro</i>: <b>{err}</b>")
     else:
