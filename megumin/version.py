bf0c42fe0d261df45703051a0fae3b87d82771c8
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2023-05-03 21:14:07 -0300

    Updates in SDL and MORE... start 2.3.1 version

--- megumin/version.py
+++ megumin/version.py
@@ -5,6 +5,6 @@
 from sys import version_info
 from pyrogram import __version__
 
-__megumin_version__ = "v2.2.1"
+__megumin_version__ = "v2.3.1"
 __python_version__ = f"{version_info[0]}.{version_info[1]}.{version_info[2]}"
 __pyro_version__ = __version__
