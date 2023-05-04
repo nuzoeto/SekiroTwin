ac18f897214fde3a969387f9341ca1ac034a52f8
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2023-05-03 23:23:13 -0300

    Update ytdl.py

--- megumin/modulos/ytdl.py
+++ megumin/modulos/ytdl.py
@@ -14,6 +14,7 @@ import contextlib
 
 
 from yt_dlp import YoutubeDL
+from urllib.parse import unquote
 from typing import Tuple, Callable
 from functools import wraps, partial
 from bs4 import BeautifulSoup
