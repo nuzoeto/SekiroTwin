849166a4ce4101a95b1c50d7cbbb4dcf49b2f548
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2023-05-04 18:11:40 -0300

    Update ytdl.py

--- megumin/modulos/ytdl.py
+++ megumin/modulos/ytdl.py
@@ -353,10 +353,11 @@ async def cli_ytdl(c: megux, cq: CallbackQuery):
     shutil.rmtree(tempdir, ignore_errors=True)
 
     
-@megux.on_message(filters.command(["sdl", "mdl", "dl"]) | filters.regex(SDL_REGEX_LINKS), group=17)
+@megux.on_message(filters.command(["sdl", "mdl", "dl"]))
+@megux.on_message(filters.regex(SDL_REGEX_LINKS))
 async def sdl(c: megux, m: Message):
     if m.matches:
-        if m.chat.type == enums.ChatType.PRIVATE or await csdl(m.chat.id) == True:
+        if m.chat.type is enums.ChatType.PRIVATE or await csdl(m.chat.id) == True:
             url = m.matches[0].group(0)
         else:
             return
