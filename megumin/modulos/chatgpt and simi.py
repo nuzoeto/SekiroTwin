2b56fd8502d1bd819a32c20ee6314a17e209eaf5
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2023-05-25 15:11:59 -0300

    Add chat gpt requeriments

--- megumin/modulos/chatgpt and simi.py
+++ megumin/modulos/chatgpt and simi.py
@@ -1,6 +1,5 @@
 import requests 
-
-from chatgpt import ChatGPT
+import openai
 
 from pyrogram import filters 
 from pyrogram.types import Message 
@@ -9,8 +8,20 @@ from pyrogram.enums import ChatType
 from megumin import megux, Config
 from megumin.utils import get_collection, get_string 
 
+openai.api_key = Config.API_CHATGPT
 
- 
+async def generate_response(text):
+    response = await openai.Completion.create(
+        engine='text-davinci-003',  # Especifique o modelo do ChatGPT a ser usado
+        prompt=question,  # O texto de entrada ou pergunta para o modelo
+        max_tokens=50,  # O número máximo de tokens para a resposta gerada
+        n=1,  # O número de respostas a serem geradas
+        stop=None,  # Um token opcional para indicar o fim da resposta gerada
+    )
+
+    answer = response.choices[0].text.strip()  # Obtém a resposta gerada do ChatGPT
+    return answer
+
 
 @megux.on_message(filters.command("simi", Config.TRIGGER))
 async def simi_(_, m: Message):
@@ -34,5 +45,5 @@ async def simi_(_, m: Message):
 async def chatgpt(c: megux, m: Message):
     args = m.text.split()[:1]
     text = " ".join(args)
-    response = await chat_gpt.generate_response(text)
+    response = await generate_response(text)
     await m.reply(response)
