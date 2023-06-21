6f13d5e054f4fc70ab52bb947df90bca8b42db01
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2023-06-21 07:09:58 -0300

    Update __init__.py

--- megumin/utils/__init__.py
+++ megumin/utils/__init__.py
@@ -1,6 +1,7 @@
 from .database import get_collection
 from .database.lang import get_string, add_lang # nao_meche
 from .database.lang import get_string as tld # nao_meche
+from .database.antiflood import rflood  # nao_meche
 from .database.disabled import is_disabled, is_disabled_user # nao_meche
 from .database.ytdl import csdl, cisdl, tsdl, tisdl #nao_meche
 from .database.afk import check_afk
