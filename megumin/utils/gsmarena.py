5b59f5e60e510aa2051c73ea09a6e2b869af8afd
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2023-06-22 22:50:36 -0300

    Update gsmarena.py

--- megumin/utils/gsmarena.py
+++ megumin/utils/gsmarena.py
@@ -10,20 +10,18 @@ def search(query):
     results = []
     for li in soup.select(".makers ul li"):
         name = li.a.text.strip()
-        link = li.a["href"]
         image = li.img["src"]
+        link = li.a["href"]
         id = link.split("-")[0].split("_")[-1]
-
         result = {
             "name": name,
-            "id": id,
+            "image": image,
             "link": link,
-            "image": image
+            "id": id
         }
         results.append(result)
 
-    return json.dumps(results)
-
+    return results
 
 def device_info(link):
     url = f"https://www.gsmarena.com/{link}"
@@ -32,23 +30,21 @@ def device_info(link):
 
     specs_list = soup.find("div", class_="specs-list")
     if specs_list:
-        battery = specs_list.find("span", attrs={"data-spec": "batdescription1"}).find_next_sibling("strong").text.strip()
-        processor = specs_list.find("span", attrs={"data-spec": "cpu"}).find_next_sibling("strong").text.strip()
-        ram = specs_list.find("span", attrs={"data-spec": "internalmemory"}).find_next_sibling("strong").text.strip()
-        connection = specs_list.find("span", attrs={"data-spec": "networkspeed"}).find_next_sibling("strong").text.strip()
-        wifi = specs_list.find("span", attrs={"data-spec": "wlan"}).find_next_sibling("strong").text.strip()
-        internal_memory = specs_list.find("span", attrs={"data-spec": "internalmemory"}).find_next_sibling("strong").text.strip()
+        processor = specs_list.find("td", text="CPU").find_next("td").text.strip()
+        battery = specs_list.find("td", text="Battery").find_next("td").text.strip()
+        ram = specs_list.find("td", text="RAM").find_next("td").text.strip()
+        wifi = specs_list.find("td", text="Wi-Fi").find_next("td").text.strip()
+        connection = specs_list.find("td", text="Network").find_next("td").text.strip()
+        internal_memory = specs_list.find("td", text="Internal").find_next("td").text.strip()
 
         info = {
-            "battery": battery,
             "processor": processor,
+            "battery": battery,
             "ram": ram,
-            "connection": connection,
             "wifi": wifi,
+            "connection": connection,
             "internal_memory": internal_memory
         }
 
-        return json.dumps(info)
-
-    return None
+        return info
 
