12d83c61cc1920889bbaa63fa6fe38292a3e149f
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2023-06-23 18:27:36 -0300

    Update deviceinfo.py

--- megumin/modulos/deviceinfo.py
+++ megumin/modulos/deviceinfo.py
@@ -25,15 +25,15 @@ async def deviceinfo(c: megux, m: Message):
             id = get_search_api[0]["id"]
             try:
                 name_cll = name or None
-                s1 = get_search_api["processor"]
+                s1 = get_search_api[0]["processor"]
                 s1_name = "Processador"
-                s2 = get_search_api["ram"]
+                s2 = get_search_api[0]["ram"]
                 s2_name = "Memoria Ram"
-                s3 = get_search_api["wifi"]
-                s3_name = "Banda Wi-Fi"
-                s4 = get_search_api["connection"]
-                s4_name = "Conexão Movel"
-                s5 = get_search_api["battery"]
+                s3 = get_search_api[0]["internal_storage"]
+                s3_name = "Memória interna"
+                s4 = get_search_api[0]["description"]
+                s4_name = "Descrição"
+                s5 = get_search_api[0]["battery"]
                 s5_name = "Bateria"
                 await m.reply(f"<b>Foto Device</b>: {img}\n<b>URL Fonte:</b>: https://www.gsmarena.com/{id}.php\n\n<b>- Aparelho</b>:  <i>{name_cll}</i>\n<b>- {s1_name}</b>: <i>{s1}</i>\n<b>- {s2_name}</b>: <i>{s2}</i>\n<b>- {s3_name}</b>: <i>{s3}</i>\n<b>- {s4_name}</b>: <i>{s4}</i>\n<b>- {s5_name}</b>: <i>{s5}</i>\n\n<b>Descrição</b>: {description}", disable_web_page_preview=False)
             except Exception as err:
