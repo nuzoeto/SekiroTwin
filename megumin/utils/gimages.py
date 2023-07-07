8af1168111737c7a38b3f5657a3f668a6ec19de9
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2023-07-07 19:28:05 -0300

    Update gimages.py

--- megumin/utils/gimages.py
+++ megumin/utils/gimages.py
@@ -10,7 +10,7 @@ class GoogleImagesAPI:
     def __init__(self):
         self.results = []
         self.headers = {
-            f"User-Agent": "Mozilla/5.0 (Windows NT 10.0;Win64(Build: {uuid4()})) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+            "User-Agent": f"Mozilla/5.0 (Windows NT 10.0;Win64(Build: {uuid4()})) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
         }
 
     def resolution(self, url):
