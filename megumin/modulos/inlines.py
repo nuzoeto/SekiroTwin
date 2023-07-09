d3921d4a8194ac5d8add877719174df06d906525
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2023-07-09 14:09:25 -0300

    Update inlines.py

--- megumin/modulos/inlines.py
+++ megumin/modulos/inlines.py
@@ -128,7 +128,7 @@ async def images(c: megux, q: InlineQuery):
     try:
         query = q.query.split(maxsplit=1)[1]
         user_id = q.from_user.id
-        res = pixabay.pixabay_results_photo(query, 200)
+        res = pixabay.pixabay_results_photo(query, 5)
     except Exception:
         return
     await q.answer(
