b3acc42cb3c52af206d7528c6bb8342c85442cd8
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2023-07-09 23:25:41 -0300

    Update and rename pixabay.py to pexels.py

--- megumin/utils/pexels.py
+++ megumin/utils/pexels.py
@@ -4,35 +4,37 @@ from pyrogram.types import InlineQueryResultPhoto
 
 from megumin import Config
 
-class PixabayImagesAPI:
+class PexelsImagesAPI:
     def __init__(self):
-        self.pixabay_results = []
-        self.pixabay_api_key = Config.PIXABAY_API_KEY
-        self.pixabay_url = "https://pixabay.com/api/"
+        self.pexels_results = []
+        self.pexels_api_key = Config.PEXELS_API_KEY
+        self.pexels_url = "https://api.pexels.com/v1/search"
 
     def pixabay_image(self, query: str, per_page: int):
+        headers = {
+            "Authorization": self.pexels_api_key
+        }
         params = {
-            "key": self.pixabay_api_key,
-            "q": query,
+            "query": query,
             "per_page": per_page,
         }
         
-        res = requests.get(self.pixabay_url, params=params)
+        res = requests.get(self.pexels_url, headers=headers, params=params)
         data = res.json()
 
         images = []
-        for image in data["hits"]:
-            images.append(image["largeImageURL"])
+        for image in data["photo"]:
+            images.append(image["src"]["large"])
         return images
 
     def pixabay_results_photo(self, query: str, per_page: int):
         search_results = self.pixabay_image(query, per_page)
         for i, image_url in enumerate(search_results):
-            self.pixabay_results.append(
+            self.pexels_results.append(
                 InlineQueryResultPhoto(
                     photo_url=image_url,
                     thumb_url=image_url,
                 )
             )
-        return self.pixabay_results
+        return self.pexels_results
         
