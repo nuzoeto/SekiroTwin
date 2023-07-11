96e7627bd5c427d24638237a78e3880aab9836e7
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2023-07-11 15:11:19 -0300

    Update config.py

--- megumin/config.py
+++ megumin/config.py
@@ -28,7 +28,7 @@ class Config:
     ADMINS = {}
     LASTFM_API_KEY = os.environ.get("LASTFM_API_KEY")
     API_ID = int(os.environ.get("API_ID"))
-    RAM_CHECK = "1024"
+    RAM_CHECK = "1"
     CPU_MHZ_CHECK = "1000"
     STORAGE_CHECK = "20"
     MIN_SYSTEM = "20.04"
