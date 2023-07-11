7f1bee0e2e2afb4eae80ca9cbac1ab04d9f39813
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2023-07-11 15:16:52 -0300

    Create check.py

---
+++ megumin/utils/check.py
@@ -0,0 +1,28 @@
+import psutil
+import platform
+
+from megumin import Config
+
+def check_pc():
+    # Verifica a quantidade de RAM
+    ram = psutil.virtual_memory().total / (1024 ** 3)  # em GB
+    if ram < Config.RAM_CHECK:
+        return False
+
+    # Verifica a velocidade do processador
+    freq = psutil.cpu_freq().current  # em MHz
+    if freq < Config.CPU_MHZ_CHECK:
+        return False
+
+    # Verifica a versão do sistema operacional
+    version = platform.release()
+    if version < Config.MIN_SYSTEM:
+        return False
+
+    # Verifica o espaço livre em disco
+    disk = psutil.disk_usage('/').free / (1024 ** 3)  # em GB
+    if disk < Config.STORAGE_CHECK:
+        return False
+
+    return True
+
