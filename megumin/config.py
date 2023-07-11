5629231590da09200cdc976ad40814dab62fc534
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2023-07-11 15:04:03 -0300

    Update config.py

--- megumin/config.py
+++ megumin/config.py
@@ -30,7 +30,7 @@ class Config:
     API_ID = int(os.environ.get("API_ID"))
     RAM_CHECK = "1024"
     CPU_MHZ_CHECK = "1000"
-    STORAGE_CHECK = "22000"
+    STORAGE_CHECK = "20000"
     API_HASH = os.environ.get("API_HASH")
     BOT_TOKEN = os.environ.get("BOT_TOKEN")
     GP_LOGS = int(os.environ.get("GP_LOGS"))
