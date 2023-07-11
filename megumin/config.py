c15b9c6e4a5ecbfb56def1c893ae9b4b3aa69a9d
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2023-07-11 16:26:17 -0300

    Update config.py

--- megumin/config.py
+++ megumin/config.py
@@ -31,7 +31,7 @@ class Config:
     RAM_CHECK = 1
     CPU_MHZ_CHECK = 1000
     STORAGE_CHECK = 20
-    MIN_SYSTEM = "5.10"
+    MIN_SYSTEM = "5.10.0"
     WSL_SYSTEM = "5.10"
     API_HASH = os.environ.get("API_HASH")
     BOT_TOKEN = os.environ.get("BOT_TOKEN")
