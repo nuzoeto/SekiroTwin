9870e15085a9123cde08b004d7409affbde64d19
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2023-07-11 16:15:53 -0300

    Update check.py

--- megumin/utils/check.py
+++ megumin/utils/check.py
@@ -16,6 +16,17 @@ def check_requirements():
     if freq < Config.CPU_MHZ_CHECK:
         return False
 
+    # Verifica a versão do sistema operacional
+    version = platform.release()
+    if 'microsoft' in platform.uname().release.lower():
+        # Executando no Ubuntu no WSL
+        if LooseVersion(version.split('-')[0]) < LooseVersion(Config.WSL_SYSTEM):
+            return False
+    else:
+        # Executando no Ubuntu normal
+        if LooseVersion(version) < LooseVersion(Config.MIN_SYSTEM):
+            return False
+
     # Verifica o espaço em disco
     disk = psutil.disk_usage('/').total / (1024 ** 3)  # em GB
     if disk < Config.STORAGE_CHECK:
