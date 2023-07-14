340f4c3f1adb43402404ea1c69592208ff8c8c3b
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2023-07-13 23:05:59 -0300

    Update wiki.py

--- megumin/modulos/wiki.py
+++ megumin/modulos/wiki.py
@@ -7,27 +7,109 @@ import wikipedia
 import re
 
 from pyrogram import filters
-from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.types import (
+    InlineQuery,
+    InlineQueryResultArticle,
+    InputTextMessageContent,
+    InlineKeyboardMarkup, 
+    InlineKeyboardButton, 
+    Message,
+)
+
+from typing import Union
 
 from megumin import megux, Config
 from megumin.utils import tld 
-from megumin.utils.decorators import input_str 
 
 
 @megux.on_message(filters.command("wiki", Config.TRIGGER))
-async def wikipt(c: megux, m: Message):
-    if not input_str(m):
-        await m.reply("Ei! Você parado cade os argumentos? Você esqueceu.")
-        return
-    query = m.text 
+@megux.on_inline_query(filters.regex(r"^wiki"))
+async def wiki(c: megux, m: Union[InlineQuery, Message]):
+    query = m.text if isinstance(m, Message) else m.query
+    chat_id = m.chat.id if isinstance(m, Message) else m.from_user.id
+    if len(text.split(maxsplit=1)) == 1:
+        try:
+            if isinstance(m, Message):
+                return await m.reply_text("Você não especificou o que deseja buscar. Por favor especifique o que deseja buscar.")
+            return await m.answer(
+                [
+                    InlineQueryResultArticle(
+                        title="wiki <query>",
+                        thumb_url=url_thumb,
+                        input_message_content=InputTextMessageContent(
+                            message_text="Você não especificou o que deseja buscar. Por favor digite o que deseja pesquisar no wiki Inline",
+                        ),
+                    )
+                ],
+                cache_time=0,
+            )
+        except BadRequest:
+            return
     kueri = re.split(pattern="wiki", string=query)
     try:
-        wikipedia.set_lang(await tld(m.chat.id, "language"))
+        wikipedia.set_lang(await tld(chat_id, "language"))
         keyboard = [[InlineKeyboardButton(text=await tld(m.chat.id, "MOREINFO_BNT"), url=wikipedia.page(kueri).url)]]
-        await m.reply((await tld(m.chat.id, "WIKI_RESULT")).format(wikipedia.summary(kueri, sentences=2)), reply_markup=InlineKeyboardMarkup(keyboard))
+        if isinstance(m, Message):
+            await m.reply((await tld(chat_id, "WIKI_RESULT")).format(wikipedia.summary(kueri, sentences=2)), reply_markup=InlineKeyboardMarkup(keyboard))
+        else:
+            res = (await tld(chat_id, "WIKI_RESULT")).format(wikipedia.summary(kueri, sentences=2))
+            await m.answer(
+                [
+                    InlineQueryResultArticle(
+                        title=(wikipedia.summary(kueri, sentences=2)),
+                        description=("Resultados para '{search}'").format(
+                           search=kueri
+                        ),
+                        input_message_content=InputTextMessageContent(
+                            message_text=res,
+                            reply_markup=keyboard,
+                        ),
+                    )
+                ],
+                cache_time=0,
+            )
     except wikipedia.PageError as e:
-        return await m.reply("error: {}".format(e))
+        if isinstance(m, Message):
+            return await m.reply("error: {}".format(e))
+        return await m.answer(
+                [
+                    InlineQueryResultArticle(
+                        title="error: {}".format(e),
+                        thumb_url=url_thumb,
+                        input_message_content=InputTextMessageContent(
+                            message_text="error: {}".format(e),
+                        ),
+                    )
+                ],
+                cache_time=0,
+            )
     except BadRequest as et:
-        return await m.reply("error: {}".format(et))
+        if isinstance(m, Message):
+            return await m.reply("error: {}".format(et))
+        return await m.answer(
+                [
+                    InlineQueryResultArticle(
+                        title="error: {}".format(et),
+                        thumb_url=url_thumb,
+                        input_message_content=InputTextMessageContent(
+                            message_text="error: {}".format(et),
+                        ),
+                    )
+                ],
+                cache_time=0,
+            )
     except wikipedia.exceptions.DesambiguationError as eet:
-        return await m.reply("⚠ Error\n Há muitas coisa! Expresse melhor para achar o resultado!\nPossíveis resultados da consulta:\n{}".format(eet))
+        if isinstance(m, Message):
+            return await m.reply("⚠ Error\nHá muitas coisas! Expresse melhor para achar o resultado!\nPossíveis resultados da consulta:\n{}".format(eet))
+        return await m.answer(
+                [
+                    InlineQueryResultArticle(
+                        title="Multiplos resultados".format(e),
+                        thumb_url=url_thumb,
+                        input_message_content=InputTextMessageContent(
+                            message_text="⚠ Error\nHá muitas coisas! Expresse melhor para achar o resultado!\nPossíveis resultados da consulta:\n{}".format(eet),
+                        ),
+                    )
+                ],
+                cache_time=0,
+            )
