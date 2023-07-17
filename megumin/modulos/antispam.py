c0fc4ffa17d6e1425ba2c404db9181979007ab03
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2023-07-17 12:57:01 -0300

    Add on, off, true and false

---
+++ megumin/modulos/antispam.py
@@ -0,0 +1,29 @@
+from pyrogram import filters
+from pyrogram.types import Message
+
+from megumin import megux
+from megumin.utils import check_rights, get_collecton, is_dev, tld
+
+@megux.on_message(filters.command(["antispam on", "antispam true"], Config.TRIGGER) & filters.group)
+async def enable_welcome_message(c: megux, m: Message):
+    db = get_collection("ANTISPAM_CHATS")
+    if not await check_rights(m.chat.id, m.from_user.id, "can_change_info"):
+        return
+    await db.update_one({"chat_id": m.chat.id}, {"$set": {"status": True}}, upsert=True)
+    await m.reply_text("Boas Vindas agora está Ativadas.")
+    
+    
+@megux.on_message(filters.command(["welcome off", "welcome false"], Config.TRIGGER) & filters.group)
+async def enable_welcome_message(c: megux, m: Message):
+    db = get_collection("ANTISPAM_CHATS")
+    if not await check_rights(m.chat.id, m.from_user.id, "can_change_info"):
+        return
+    await db.update_one({"chat_id": m.chat.id}, {"$set": {"status": False}}, upsert=True)
+    await m.reply_text("Boas Vindas agora está Desativadas.")
+    
+    
+@megux.on_message(filters.command("welcome", Config.TRIGGER) & filters.group)
+async def enable_welcome_message(c: megux, m: Message):
+    if not await check_rights(m.chat.id, m.from_user.id, "can_change_info"):
+        return
+    await m.reply_text("Dê um argumento exemplo: /welcome on/off/true/false")
