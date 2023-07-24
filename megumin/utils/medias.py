98dab7f3c8dcd84f9b34805658bdd0a36cf9430f
nuzoeto <icarocostagoncalves@gmail.com>
2023-07-24 20:00:03 -0300

    back to 0

--- megumin/utils/medias.py
+++ megumin/utils/medias.py
@@ -12,17 +12,20 @@ from bs4 import BeautifulSoup as bs
 from httpx import AsyncClient
 from yt_dlp import YoutubeDL
 
-from .tools import http, aiowrap
+from .tools import http, aiowrap          
 
 proxys = {
-    "PROXIES": ["https://95.216.17.79:3888"]
+    "PROXIES":
+    [
+        "https://95.216.17.79:3888"
+    ]
+    
 }
-
+          
 
 @aiowrap
 def extract_info(instance: YoutubeDL, url: str, download=True):
     return instance.extract_info(url, download)
-
 
 class DownloadMedia:
     def __init__(self):
@@ -66,12 +69,9 @@ class DownloadMedia:
         Returns:
             Dict: Media url and res info.
         """
-        url_str = str(url)
-        file_bytes = io.BytesIO((await http.get(url_str)).content)
-        file_bytes.name = f"{url_str[60:80]}.{filetype.guess_extension(file_bytes)}"
-        self.files.append({"p": file_bytes, "w": width, "h": height}
-
-
+        file = io.BytesIO((await http.get(url)).content)
+        file.name = f"{url[60:80]}.{filetype.guess_extension(file)}"
+        self.files.append({"p": file, "w": width, "h": height})
 
     async def Instagram(self, url: str, captions: str):
         headers = {
