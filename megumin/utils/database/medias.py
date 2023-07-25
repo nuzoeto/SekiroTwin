8f439a50bf22ff44b1af9c5a90e5b77d929f948c
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2023-07-25 10:13:22 -0300

    Update medias.py

--- megumin/utils/database/medias.py
+++ megumin/utils/database/medias.py
@@ -3,21 +3,21 @@ from megumin.utils import get_collection
 from typing import Optional
 
 
-async def csdl(gid: int) -> bool:
+async def csdl(ugid: int) -> bool:
     CSDL = get_collection(f"MEDIAS")
-    resp = await CSDL.find_one({"chat_id": gid, "status": True})
+    resp = await CSDL.find_one({"chat_id": ugid, "status": True})
     return bool(resp)
 
-async def tsdl(gid: int, mode: Optional[bool]) -> None:
+async def tsdl(ugid: int, mode: Optional[bool]) -> None:
     CSDL = get_collection(f"MEDIAS") 
-    await CSDL.update_one({"chat_id": gid}, {"$set": {"status": mode}}, upsert=True)  
+    await CSDL.update_one({"chat_id": ugid}, {"$set": {"status": mode}}, upsert=True)  
     
-async def cisdl(gid: int) -> bool:
+async def cisdl(ugid: int) -> bool:
     CISDL = get_collection(f"CAPTION_SDL")
-    row = await CISDL.find_one({"status": True})
+    row = await CISDL.find_one({"chat_id": ugid, "status": True})
     return bool(row)
 
-async def tisdl(gid: int, mode: Optional[bool]) -> None:
+async def tisdl(ugid: int, mode: Optional[bool]) -> None:
     CISDL = get_collection(f"CAPTION_SDL")
-    await CSDL.update_one({"chat_id": gid}, {"$set": {"status": mode}}, upsert=True)
+    await CSDL.update_one({"chat_id": ugid}, {"$set": {"status": mode}}, upsert=True)
     
